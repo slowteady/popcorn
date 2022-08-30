53fa27a1790a8b51dc2b9d7f658cdf2d3d01d349
LeeYongMin01 <vbn0213@gmail.com>
2022-08-30 20:18:31 +0900

    index 수정 및 render 방식 변경

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    next();
-});
-
-router.get('/', (req, res, next) => {
-    res.send('POPCORN!');
+    res.render('index', {title: 'POPCORN!'});
 });
 
 module.exports = router;
