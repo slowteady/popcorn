ae021e8e3ae45a8c9ff775e78f8e586645914269
LeeYongMin01 <vbn0213@gmail.com>
2022-08-30 19:49:09 +0900

    route 설정

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+    next();
+});
+
+router.get('/', (req, res, next) => {
     res.send('POPCORN!');
 });
 
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.send('HELLO USER!');
 });
 
+router.get('/:id', (req, res) => {
+    console.log(req.params, req.query);
+});
+
 module.exports = router;
