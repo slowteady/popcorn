6c9bde3dd49a202d4ef230440b175db165e9886c
LeeYongMin01 <vbn0213@gmail.com>
2022-08-30 19:13:18 +0900

    index를 router페이지로 변경중

--- index.js
+++ index.js
@@ -1,63 +1,9 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const path = require('path');
 
-dotenv.config();
-const app = express();
-app.set('port', process.env.PORT || 3000);
+const router = express.Router();
 
-app.use((req, res, next) => {
-    if(process.env.NODE_ENV === 'production') {
-        morgan('combined')(req, res, next);
-    } else {
-        morgan('dev')(req, res, next);
-    }
+router.get('/', (req, res) => {
+    res.send('POPCORN!');
 });
 
-app.use((req, res, next) => {
-    console.log('요청 실행');
-    next();
-});
-
-app.use(
-    morgan('dev'),
-    // express.static('/', path.join(__dirname, 'public')),
-    express.json(),
-    express.urlencoded({ extended: false }),
-    cookieParser(process.env.COOKIE_SECRET)
-);
-
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false
-    },
-    name: 'session-cookie'
-}));
-
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-    next();
-});
-
-app.use((req, res, next) => {
-    next();
-}, (req, res) => {
-    throw new Error('send to error middleware');
-});
-
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
-});
-
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '포트 ON');
-});
-
+module.exports = router;
