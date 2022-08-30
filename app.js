8ada9a6b908d6ed3f40724fe8597af2b23ec1155
LeeYongMin01 <vbn0213@gmail.com>
2022-08-30 19:30:25 +0900

    express 재설정

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
+const app = express();
+let port = 3000;
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`${port} port is on!!`);
+});
