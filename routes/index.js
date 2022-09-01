88d8c5c29ae7056c98dbb062a70bfa2fe15cf50f
LeeYongMin01 <vbn0213@gmail.com>
2022-09-01 20:05:07 +0900

    부트스트랩 추가

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('POPCORN!');
-    // res.render('index', {title: 'POPCORN!'});
+    res.sendFile(path.join(__dirname, '../views/html/login.html'));
 });
 
 module.exports = router;
