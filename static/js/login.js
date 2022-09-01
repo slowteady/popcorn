584d33bc743891a41ac7337e995a041dbdd3b8ce
LeeYongMin01 <vbn0213@gmail.com>
2022-09-02 00:32:12 +0900

    static 폴더로 변경, login 페이지 진행중

---
+++ static/js/login.js
@@ -0,0 +1,25 @@
+const createIndexDOM = () => {
+  let array = [];
+
+  let template = `
+    <div class="loginBox">
+        <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
+            <h3>Sign in here</h3>
+            <form action="" method="post">
+                <div class="inputBox">
+                    <input id="uname" type="text" name="Username" placeholder="Username"> 
+                    <input id="pass" type="password" name="Password" placeholder="Password"> 
+                </div>
+                <input type="submit" name="" value="Login">
+            </form> 
+            <a href="#">Forget Password<br></a>
+            <div class="text-center">
+                <p style="color: #59238F;">Sign-Up</p>
+            </div>
+        </div>
+    `;
+
+  document.body.innerHTML = template;
+};
+
+createIndexDOM();
