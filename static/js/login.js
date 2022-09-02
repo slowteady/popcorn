e6ee4a2f13ddc8135a805f29e79f306f45e666f2
LeeYongMin01 <vbn0213@gmail.com>
2022-09-02 13:04:00 +0900

    요소 변경

--- static/js/login.js
+++ static/js/login.js
@@ -3,20 +3,22 @@ const createIndexDOM = () => {
 
   let template = `
     <div class="loginBox">
-        <img class="user" src="https://i.ibb.co/yVGxFPR/2.png" height="100px" width="100px">
-            <h3>Sign in here</h3>
-            <form action="" method="post">
-                <div class="inputBox">
-                    <input id="uname" type="text" name="Username" placeholder="Username"> 
-                    <input id="pass" type="password" name="Password" placeholder="Password"> 
-                </div>
-                <input type="submit" name="" value="Login">
-            </form> 
-            <a href="#">Forget Password<br></a>
-            <div class="text-center">
-                <p style="color: #59238F;">Sign-Up</p>
-            </div>
+        <div class="iconBox">
+            <img class="icon" src="../img/popcorn_Icon.jpg" height="100px" width="100px">
         </div>
+        <h3>Sign in here</h3>
+        <form action="" method="post">
+            <div class="inputBox">
+                <input id="uname" type="text" name="Username" placeholder="Username"> 
+                <input id="pass" type="password" name="Password" placeholder="Password"> 
+            </div>
+            <input type="submit" name="" value="Login">
+        </form> 
+        <a href="#">Forget Password<br></a>
+        <div class="text-center">
+            <p style="color: #59238F;">Sign-Up</p>
+        </div>
+    </div>
     `;
 
   document.body.innerHTML = template;
