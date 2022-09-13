b760a15962686b85f53f3a1ef826c24efdf17f27
LeeYongMin01 <vbn0213@gmail.com>
2022-09-13 22:50:11 +0900

    회원가입 페이지 진행중, 구조 완성 후 분기처리 필요

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,8 @@
-const createIndexDOM = () => {
+// 로그인 DOM
+const createloginDOM = () => {
   let array = [];
 
-  let template = `
+  let loginTemplate = `
     <div class="loginBox">
         <div class="iconBox">
             <img class="icon" src="../img/popcorn_Icon.jpg" height="100px" width="100px">
@@ -9,10 +10,10 @@ const createIndexDOM = () => {
         <div class="title" id="title"></div>
         <form action="" method="post">
             <div class="inputBox">
-                <input id="uname" type="text" name="Username" placeholder="Username"> 
+                <input id="uname" type="text" name="Id" placeholder="Email or Username"> 
                 <input id="pass" type="password" name="Password" placeholder="Password"> 
             </div>
-            <input type="submit" name="" value="Login">
+            <input type="submit" value="Login">
         </form> 
         <div class="pwBox">
             <span class="pw"></span>
@@ -23,27 +24,55 @@ const createIndexDOM = () => {
     </div>
     `;
 
-  document.body.innerHTML = template;
+  document.body.innerHTML = loginTemplate;
 };
 
+// 회원가입 모달 창
+const createSignUpModal = () => {
+    let array = [];
+    let signUpTemplate = `
+        <div class="signUpBox">
+            <div class="wrapBox">
+                <form action="" method="post">
+                    <div class="inputBox">
+                        <div class="label" id="idLabel">Email</div>
+                            <input id="email" type="text" placeholder="Email">
+                        <div class="label" id="emailLabel">Phone</div>
+                            <input id="phone" type="text" placeholder="Phone">
+                        <div class="label" id="emailLabel">Username</div>
+                            <input id="username" type="text" placeholder="Username">
+                    </div>
+                    <input class="signup_btn" type="submit" value="Sign Up">
+                </form>    
+            </div>      
+        </div>
+    `;
+    document.body.innerHTML = signUpTemplate;
+}
+
+// Text SetValue
 const textSet = () => {
     const span = document.createElement('span');
 
     const title = document.getElementById('title');
     title.innerText = 'POPCORN!';
 
+    // 비밀번호 찾기
     const pw = document.querySelector('.pw');
     pw.innerText = 'Forget Password?';
     pw.addEventListener('click', () => {
         alert('서비스 준비중입니다');
     });
 
+    // 회원가입
     const tc = document.querySelector('.tc');
     tc.innerText = 'Sign-Up';
     tc.addEventListener('click', () => {
-        
+        const loginBox = document.querySelector('.loginBox');
+        loginBox.classList.add('hidden');
+        createSignUpModal();
     });
 }
 
-createIndexDOM();
+createloginDOM();
 textSet();
