ab02fb06e5f0f40eedb5d6d07dc8dc005168b608
LeeYongMin01 <vbn0213@gmail.com>
2022-09-13 19:00:42 +0900

    로그인 페이지 css 수정

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('HELLO USER!');
+    
 });
 
 router.get('/:id', (req, res) => {
