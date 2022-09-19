1349d3ac5a725cdac53780e2a40275b3a33790aa
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 23:00:53 +0900

    body-parser 수정

--- app/app.js
+++ app/app.js
@@ -14,14 +14,14 @@ nunjucks.configure('./app/src/views', {
     watch: true
 });
 
-app.use('/', indexRouter);
+app.use(bodyParser.json());
+// 한글, 공백이 포함될 경우 정상적으로 인식되지 않는 문제를 도와줌
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/src/static'));
-app.use(bodyParser.json());
 
-// 한글, 공백이 포함될 경우 정상적으로 인식되지 않는 문제를 도와줌
-app.use(bodyParser.urlencoded({extended: true }));
-
+app.use('/', indexRouter);
 app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
