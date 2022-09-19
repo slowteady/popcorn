9f2dd7ecc5f1438e49a10694212deaaf3171fe60
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 19:48:59 +0900

    파일 분리

---
+++ app/src/static/js/login.js
@@ -0,0 +1,15 @@
+// 비밀번호 찾기
+const pw = document.querySelector('.pw');
+pw.addEventListener('click', () => {
+    alert('서비스 준비중입니다');
+});
+
+const tc = document.querySelector('.tc');
+tc.addEventListener('click', () => {
+    
+});
+
+// const loginBtn = document.querySelector('#loginBtn');
+// loginBtn.addEventListener('click', () => {
+    
+// });
