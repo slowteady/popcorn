760b330e062f077ee9eb1c20a7442ff39fb98ced
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 22:33:53 +0900

    로그인 페이지 진행중

--- app/src/static/js/login.js
+++ app/src/static/js/login.js
@@ -4,12 +4,18 @@ pw.addEventListener('click', () => {
     alert('서비스 준비중입니다');
 });
 
-const tc = document.querySelector('.tc');
-tc.addEventListener('click', () => {
+const signUpText = document.querySelector('.signUpText');
+signUpText.addEventListener('click', () => {
     
 });
 
-// const loginBtn = document.querySelector('#loginBtn');
-// loginBtn.addEventListener('click', () => {
-    
-// });
+const login = () => {
+    const req = {
+        id: id.value,
+        password: password.value
+    };
+    console.log(req);
+};
+
+const loginBtn = document.querySelector('.loginBtn');
+loginBtn.addEventListener('click', login);
