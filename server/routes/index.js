c6b9d705408d14bd02b282b67fd33550242825b4
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 17:27:17 +0900

    controller 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
+const ctrl = require('../../controller/home.ctrl');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('index');
-});
+router.get('/', ctrl.index);
 
 module.exports = router;
