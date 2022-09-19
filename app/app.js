d5d1262b04f42c4458d865fdfd87469b840c1eb5
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 22:51:54 +0900

    body-parser추가

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const nunjucks = require('nunjucks');
 const morgan = require('morgan');
