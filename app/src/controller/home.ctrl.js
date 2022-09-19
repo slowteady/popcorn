d69f1f02bf90f35254ff9c9355015a02ec3666fd
LeeYongMin01 <vbn0213@gmail.com>
2022-09-19 23:17:54 +0900

    로그인 예시 추가

--- app/src/controller/home.ctrl.js
+++ app/src/controller/home.ctrl.js
@@ -9,10 +9,28 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+        password = req.body.password;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password) {
+                return res.json({
+                    success: true
+                });
+            }
+            return res.json({
+                success: false,
+                msg: "로그인 실패"
+            });
+        }
     }
 };
 
+const users = {
+    id: ["lym", "ace"],
+    password: ["123", "1234"]
+};
 
 module.exports = {
     output,
