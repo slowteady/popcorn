85fdc5e5b2e332e80e661688ee4f46954564fa9a
LeeYongMin01 <vbn0213@gmail.com>
2022-09-21 23:48:18 +0900

    컨트롤러 업데이트

--- app/src/controller/home.ctrl.js
+++ app/src/controller/home.ctrl.js
@@ -1,4 +1,5 @@
 const UserStorage = require("../model/UserStorage");
+const User = require('../model/User');
 
 const output = {
     index: (req, res) => {
@@ -11,19 +12,8 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-        password = req.body.password;
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인 실패";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     }
 };
