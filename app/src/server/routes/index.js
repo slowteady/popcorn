de931509227a16da8e7c45223f7aac398a80ea8f
LeeYongMin01 <vbn0213@gmail.com>
2022-09-22 23:58:29 +0900

    회원가입 페이지 router 추가

--- app/src/server/routes/index.js
+++ app/src/server/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', ctrl.output.index);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login);
 
 module.exports = router;
