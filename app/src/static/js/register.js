e30d7720476623679269b3acc20543aff19a55a5
LeeYongMin01 <vbn0213@gmail.com>
2022-09-25 12:19:37 +0900

    fetch 추가

--- app/src/static/js/register.js
+++ app/src/static/js/register.js
@@ -6,19 +6,42 @@ const email = document.querySelector('#email'),
 
 // SignUp
 const signUpBtn = document.querySelector('#signUpBtn');
-signUpBtn.addEventListener('click', () => {
-    const req = {
-        email: email.value,
-        username: username.value,
-        phone: phone.value,
-        password: password.value,
-        pwConfirm: pwConfirm.value,
-    };
-});
+signUpBtn.addEventListener('click', register);
 
 // Back 
 const backBtn = document.querySelector('#backBtn');
 backBtn.addEventListener('click', () => {
     location.href = "/";
 });
+
+// Sign Up
+function register() {
+    const req = {
+        email: email.value,
+        username: username.value,
+        phone: phone.value,
+        password: password.value,
+        pwConfirm: pwConfirm.value
+    };
+
+    // fetch
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(req)
+    }).then((res) => res.json())
+    .then((res) => {
+        if(res.success) {
+            location.href = "/login";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error("회원가입 중 오류 발생");
+    });
+}
+
     
