dee75584f61b8a17b5bc3564feb071207e50b513
LeeYongMin01 <vbn0213@gmail.com>
2022-09-27 18:25:03 +0900

    user 파일화

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,12 +1,8 @@
-class UserStorage {
-    static #users = {
-        id: ["lym@naver.com", "ace@naver.com"],
-        password: ["123", "1234"],
-        name: ["이용민", "안채은"]
-    };
+const fs = require('fs');
 
+class UserStorage {
     static getUsers(...fields) {
-        const users = this.#users;
+        const users = JSON.parse(data);
         const newUsers = fields.reduce((newUsers, field) => {
             if(users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -17,18 +13,21 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        fs.readFile('./app/src/databases/users.json', (err, data) => {
+            if(err) throw err;
+            const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const userInfo = Object.keys(users).reduce((newUser, info) => {
+                newUser[info] = users[info][idx];
+                return newUser;
+            }, {});
+    
+            return userInfo;
+        });
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        const users = JSON.parse(data);
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
