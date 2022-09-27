101e47c72b80d2ff47c654592115b0a5ab03d2b4
LeeYongMin01 <vbn0213@gmail.com>
2022-09-27 18:39:07 +0900

    await async 적용

--- app/src/model/User.js
+++ app/src/model/User.js
@@ -6,9 +6,9 @@ class User {
         this.body = body;
     }
 	// 로그인 
-    login() {
+   async login() {
         const client = this.body;
-        const { id, password } = UserStorage.getUserInfo(client.id);
+        const { id, password } = await UserStorage.getUserInfo(client.id);
 
         if(id) {
             if (id === client.id && password === client.password) {
--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,38 +1,39 @@
-const fs = require('fs');
+const fs = require("fs").promises;
 
 class UserStorage {
-    static getUsers(...fields) {
+  static getUsers(...fields) {
+    const users = JSON.parse(data);
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    return fs.readFile('./app/src/databases/users.json')
+      .then((data) => {
         const users = JSON.parse(data);
-        const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+          newUser[info] = users[info][idx];
+          return newUser;
         }, {});
-        return newUsers;
-    }
 
-    static getUserInfo(id) {
-        fs.readFile('./app/src/databases/users.json', (err, data) => {
-            if(err) throw err;
-            const users = JSON.parse(data);
-            const idx = users.id.indexOf(id);
-            const userInfo = Object.keys(users).reduce((newUser, info) => {
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {});
-    
-            return userInfo;
-        });
-    }
+        return userInfo;
+      })
+      .catch(console.error);
+  }
 
-    static save(userInfo) {
-        const users = JSON.parse(data);
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        return { success: true };
-    }
+  static save(userInfo) {
+    const users = JSON.parse(data);
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    return { success: true };
+  }
 }
 
 module.exports = UserStorage;
