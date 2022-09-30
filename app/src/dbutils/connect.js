b3ef5d1b283746f09129bcb487d4459f02da8ad6
LeeYongMin01 <vbn0213@gmail.com>
2022-09-30 13:47:30 +0900

    popcorn db연결

--- app/src/dbutils/connect.js
+++ app/src/dbutils/connect.js
@@ -4,10 +4,10 @@ const uri = 'mongodb+srv://admin:admin@cluster0.kntf4.mongodb.net/?retryWrites=t
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(uri);
-        console.log('MongoDB 연동 완료');
+        await mongoose.connect(uri, { dbName: 'popcorn' });
+        console.log('MongoDB 연결 완료');
     } catch (err) {
-        console.error(err);
+        console.error('MongoDB 연결 오류', err);
     }
 };
 
