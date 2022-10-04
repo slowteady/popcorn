a453ecbb224b011e9fc375a57a6f167fb8a89d8e
LeeYongMin01 <vbn0213@gmail.com>
2022-10-04 16:00:13 +0900

    주석 해제

--- app/src/server/routes/index.js
+++ app/src/server/routes/index.js
@@ -10,7 +10,7 @@ router.use(bodyParser.json());
 router.get('/', ctrl.output.index);
 router.get('/login', ctrl.output.login);
 router.get('/signup', ctrl.output.signup);
-// router.get('/list', ctrl.output.list);
+router.get('/list', ctrl.output.list);
 
 router.post('/login', ctrl.process.login);
 router.post('/signup', ctrl.process.signup);
