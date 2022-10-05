9005e094391cec21bbd2e76d10c439d917a7eca3
LeeYongMin01 <vbn0213@gmail.com>
2022-10-05 11:06:41 +0900

    util js생성

--- app/src/static/js/signup.js
+++ app/src/static/js/signup.js
@@ -14,16 +14,6 @@ backBtn.addEventListener("click", () => {
   location.href = "/";
 });
 
-// 빈 값 체크
-const check = {
-  isEmpty: (str) => {
-    return str == "" || str == undefined || str == null || str == "null";
-  },
-  isNotEmpty: (str) => {
-    return !this.isEmpty(str);
-  },
-};
-
 // 회원가입 등록 
 function signup() {
   const req = {
@@ -76,21 +66,4 @@ function signup() {
         console.error("회원가입 중 오류 발생");
       });
   }
-}
-
-// 유효성 검사 
-function validate(obj) {
-  for (let n in obj) {
-    let val = obj[n].value;
-    let name = obj[n].name;
-    if (check.isEmpty(val)) {
-      return alert(`${name}을(를) 입력해주세요`);
-    }
-  }
-
-  if (obj.password.value !== obj.confirmPw.value) {
-    return alert("패스워드와 패스워드 확인이 다릅니다");
-  }
-
-  return true;
 }
