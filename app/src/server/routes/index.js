74b63924d6246c80afc94a59c97c1a9eaba43570
LeeYongMin01 <vbn0213@gmail.com>
2022-10-05 10:40:55 +0900

    bodyparser 중복 제거

--- app/src/server/routes/index.js
+++ app/src/server/routes/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const ctrl = require('../../controller/index.ctrl');
 
 const router = express.Router();
-
-// MiddleWare
-router.use(bodyParser.json());
 
 router.get('/', ctrl.output.index);
 router.get('/login', ctrl.output.login);
