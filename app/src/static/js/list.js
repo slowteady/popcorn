f3b926dc426758c094b0c1d3605ea03322663188
LeeYongMin01 <vbn0213@gmail.com>
2022-10-10 17:40:12 +0900

    user 아이콘 추가, 이벤트 추가

--- app/src/static/js/list.js
+++ app/src/static/js/list.js
@@ -2,3 +2,8 @@ const homeBtn = document.querySelector('#homeBtn');
 homeBtn.addEventListener('click', () => {
     location.href = "/list";
 });
+
+const logoutBtn = document.querySelector('#logoutBtn');
+logoutBtn.addEventListener('click', () => {
+    location.href = "/login";
+});
