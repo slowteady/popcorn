d1e5679e4550935fbdaa9a9e6c0979e913223d2c
LeeYongMin01 <vbn0213@gmail.com>
2022-10-12 18:53:56 +0900

    아이콘 마우스 오버 확대 함수 생성

--- app/src/static/js/list.js
+++ app/src/static/js/list.js
@@ -4,6 +4,34 @@ homeBtn.addEventListener('click', () => {
 });
 
 const logoutBtn = document.querySelector('#logoutBtn');
+iconOver(logoutBtn);
 logoutBtn.addEventListener('click', () => {
     location.href = "/login";
 });
+
+const officeTxt = document.querySelector('#officeTxt');
+officeTxt.addEventListener('click', () => {
+    location.href = "/list";
+});
+
+const myPageBtn = document.querySelector('#mypage');
+iconOver(myPageBtn);
+
+function iconOver(btn) {
+    btn.addEventListener('mouseenter', (event) => {
+        zoomIn(event);
+    });
+    btn.addEventListener('mouseleave', (event) => {
+        zoomOut(event);
+    });
+}
+function zoomIn(event) {
+    event.target.style.width = "30px";
+    event.target.style.height = "35px";
+    event.target.style.transition = "all 0.2s";
+}
+function zoomOut(event) {
+    event.target.style.width = "30px";
+    event.target.style.height = "30px";
+    event.target.style.transition = "all 0.2s";
+}
