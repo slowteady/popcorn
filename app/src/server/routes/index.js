efd44b17d1fa78c483245cb340c72bc5a6e04d3f
LeeYongMin01 <vbn0213@gmail.com>
2022-10-23 14:26:33 +0900

    router 삭제

--- app/src/server/routes/index.js
+++ app/src/server/routes/index.js
@@ -7,7 +7,6 @@ router.get('/', ctrl.output.index);
 router.get('/login', ctrl.output.login);
 router.get('/signup', ctrl.output.signup);
 router.get('/list', ctrl.output.list);
-router.get('/list', ctrl.output.callApi);
 
 router.post('/login', ctrl.process.login);
 router.post('/signup', ctrl.process.signup);
