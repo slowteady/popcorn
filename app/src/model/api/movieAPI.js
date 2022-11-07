9164085e54add06caf0d48aea6081b11a8265a2f
LeeYongMin01 <vbn0213@gmail.com>
2022-11-07 21:12:45 +0900

    네이버API -> KMDB API로 변경

--- app/src/model/api/movieAPI.js
+++ app/src/model/api/movieAPI.js
@@ -7,16 +7,19 @@ async function movieApi(data) {
     const jsonArr = new Object();
 
     let name = data[i].movieNm;
+    let releaseDate = data[i].openDt;
+    let opnDate = dateFormat(new Date(releaseDate));
+    
     jsonArr.name = name;
 
-    const movieData = await callApi(name);
-    const mvData = movieData.data.items[0];
-
-    jsonArr.title = mvData.title;
-    jsonArr.image = mvData.image;
-    jsonArr.director = mvData.director;
-    jsonArr.actor = mvData.actor;
-    jsonArr.rank = data[i].rank;
+    const movieData = await callApi(name, opnDate);
+    const mvData = movieData.data.Data[0].Result[0];
+    
+    // jsonArr.title = mvData.title;
+    // jsonArr.image = mvData.image;
+    // jsonArr.director = mvData.director;
+    // jsonArr.actor = mvData.actor;
+    // jsonArr.rank = data[i].rank;
 
     arr.push(jsonArr);
   }
@@ -25,25 +28,35 @@ async function movieApi(data) {
 }
 
 // 영화 API 호출
-function callApi(name) {
-  if (!name) {
+function callApi(name, opnDate) {
+  if (!name || !opnDate) {
     return false;
   }
-  const url = "https://openapi.naver.com/v1/search/movie.json";
-  const clientId = "NTBqClUaZFMYahJjFa6B";
-  const secret = "cn0smudzqa";
+  const url = "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp";
+  const serviceKey = "6BP177BZNDL80SHH268S";
   const data = {
     params: {
-      query: name,
-    },
-    headers: {
-      "X-Naver-Client-Id": clientId,
-      "X-Naver-Client-Secret": secret,
+      title: name,
+      collection: "kmdb_new2",
+      detail: "Y",
+      releaseDts: opnDate,
+      ServiceKey: serviceKey,
     },
   };
   const promise = axios.get(url, data);
   
   return promise;
+}
+
+// date 포맷
+function dateFormat(date) {
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  month = month >= 10 ? month : '0' + month;
+  day = day >= 10 ? day : '0' + day;
+
+  return `${date.getFullYear()}${month}${day}`;
 }
 
 module.exports = movieApi;
