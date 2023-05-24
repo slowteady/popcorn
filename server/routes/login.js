a74d1dd0f95c66c96443d70a66156241a08a6153
slowteady <vbn0213@gmail.com>
2023-05-24 22:48:18 +0900

    redirect 클라이언트에서 처리할 예정

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,11 +2,6 @@
 const express = require("express");
 const router = express.Router();
 
-// 1. 로그인 전, "/"로 요청 왔을 시 로그인 페이지로 redirect
-// 2. 로그인 후, "/"로 요청 왔을 시 메인 페이지로 redirect
-router.get("/", (req, res) => res.redirect("/login"));
-
-
 
 
 
