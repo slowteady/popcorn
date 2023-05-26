35c0d0be167beb30d8f9df9af2cf6bc86286ad38
slowteady <vbn0213@gmail.com>
2023-05-26 23:21:58 +0900

    회원가입 페이지 레이아웃 구현

--- client/src/components/views/SignUpPage.tsx
+++ client/src/components/views/SignUpPage.tsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import PopcornIcon from "../../img/popcorn_icon.jpeg";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const history = useHistory();
+  const onClickCancel = () => {
+    history.push("/");
+  };
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          mt: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          sx={{ m: 1, bgcolor: "secondary.main" }}
+          src={PopcornIcon}
+          alt="popcorn_icon"
+        />
+        <Typography component="h1" variant="h5">
+          회원가입
+        </Typography>
+        <TextField
+          label="이름"
+          margin="normal"
+          name="name"
+          required
+          fullWidth
+        />
+        <TextField
+          label="이메일"
+          margin="normal"
+          autoComplete="email"
+          name="email"
+          required
+          fullWidth
+        />
+        <TextField
+          label="패스워드"
+          margin="normal"
+          type="password"
+          name="password"
+          autoComplete="current-password"
+          required
+          fullWidth
+        />
+        <TextField
+          label="패스워드 확인"
+          margin="normal"
+          type="password"
+          name="confirmPassword"
+          autoComplete="current-password"
+          required
+          fullWidth
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          sx={{ mt: 2, mb: 2 }}
+          fullWidth
+        >
+          회원가입
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          sx={{
+            backgroundColor: "#c97c63",
+            "&:hover": {
+              backgroundColor: "#ab6e59",
+            },
+          }}
+          fullWidth
+          onClick={onClickCancel}
+        >
+          취소
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default SignUpPage;
