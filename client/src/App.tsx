a5d7deaa82b5ef47fd04e904b4efae346c217627
slowteady <vbn0213@gmail.com>
2023-05-26 22:45:10 +0900

    회원가입 페이지 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "./components/views/LoginPage";
+import SignUpPage from "./components/views/SignUpPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/signup" component={SignUpPage}/>
       </Switch>
     </BrowserRouter>
   );
