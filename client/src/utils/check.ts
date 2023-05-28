127cd6825396a63ae0f34f9744af2e55ab77308d
slowteady <vbn0213@gmail.com>
2023-05-28 20:48:31 +0900

    문자 검증 로직 추가

--- client/src/utils/check.ts
+++ client/src/utils/check.ts
@@ -1,8 +1,11 @@
 export const strCheck = {
+  // 문자 있는지 검증
   isEmpty: (str: string) => {
     return str == "" || str == undefined || str == null || str == "null";
   },
+  // 문자 없는지 검증
   isNotEmpty: (str: string) => {
     return !strCheck.isEmpty(str);
   },
 }
+
