22241699caf0720774703da74691f037c1b704eb
slowteady <vbn0213@gmail.com>
2023-05-28 20:48:20 +0900

    비밀번호 일치 검증

--- client/src/components/views/SignUpPage.tsx
+++ client/src/components/views/SignUpPage.tsx
@@ -9,22 +9,23 @@ import {
   Typography,
 } from "@mui/material";
 import PopcornIcon from "../../img/popcorn_icon.jpeg";
+import Swal from "sweetalert2";
 
 interface Form {
-  name: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
+  Name: string;
+  Email: string;
+  Password: string;
+  ConfirmPassword: string;
 }
 
 // 회원가입 컴포넌트
 const SignupPage = () => {
   const history = useHistory();
-  const [formData, setFormData] = useState<Form>({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
+  const [FormData, setFormData] = useState<Form>({
+    Email: "",
+    Name: "",
+    Password: "",
+    ConfirmPassword: "",
   });
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
@@ -32,7 +33,21 @@ const SignupPage = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const { Email, Name, Password, ConfirmPassword } = FormData;
     e.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      Swal.fire({
+        icon: "error",
+        title: "비밀번호가 일치하지 않아요",
+      });
+    }
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
   };
   const onClickCancel = () => {
     history.push("/");
@@ -58,44 +73,44 @@ const SignupPage = () => {
         </Typography>
         <form onSubmit={onSubmit}>
           <TextField
-            label="이름"
-            margin="normal"
-            name="name"
-            required
-            fullWidth
-            onChange={onChangeHandler}
-            value={formData.name}
-          />
-          <TextField
             label="이메일"
             margin="normal"
             autoComplete="email"
-            name="email"
+            name="Email"
             required
             fullWidth
             onChange={onChangeHandler}
-            value={formData.email}
+            value={FormData.Email}
+          />
+          <TextField
+            label="이름"
+            margin="normal"
+            name="Name"
+            required
+            fullWidth
+            onChange={onChangeHandler}
+            value={FormData.Name}
           />
           <TextField
             label="패스워드"
             margin="normal"
             type="password"
-            name="password"
+            name="Password"
             autoComplete="current-password"
             required
             fullWidth
             onChange={onChangeHandler}
-            value={formData.password}
+            value={FormData.Password}
           />
           <TextField
             label="패스워드 확인"
             margin="normal"
             type="password"
-            name="confirmPassword"
+            name="ConfirmPassword"
             required
             fullWidth
             onChange={onChangeHandler}
-            value={formData.confirmPassword}
+            value={FormData.ConfirmPassword}
           />
           <Button
             type="submit"
