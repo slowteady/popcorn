4d680ff309c8a5d714075764191dc8d1802a1dfc
slowteady <vbn0213@gmail.com>
2023-05-30 01:34:14 +0900

    회원가입 검증 및 완료 처리 로직 추가, Swal.fire 파일로 로직 분기

--- client/src/components/auth/signupAuth.ts
+++ client/src/components/auth/signupAuth.ts
@@ -1,14 +1,17 @@
-import Swal from "sweetalert2";
 import { Form } from "../views/SignupPage";
 import { strCheck } from "../../utils/check";
+import { msg } from "../../utils/msg";
+import { Action } from "../../state/reducers/users/userReducer";
 
-export const validate = ({ Email, Name, Password, ConfirmPassword }: Form) => {
+export const inputValidate = ({
+  Email,
+  Name,
+  Password,
+  ConfirmPassword,
+}: Form) => {
   // 비밀번호, 비밀번호 확인 일치 검증
   if (Password !== ConfirmPassword) {
-    Swal.fire({
-      icon: "error",
-      title: "비밀번호가 일치하지 않아요",
-    });
+    msg("error", "비밀번호가 일치하지 않아요");
     return false;
   }
 
@@ -19,10 +22,7 @@ export const validate = ({ Email, Name, Password, ConfirmPassword }: Form) => {
     strCheck.isEmpty(Password) ||
     strCheck.isEmpty(ConfirmPassword)
   ) {
-    Swal.fire({
-      icon: "error",
-      title: "모든 정보를 입력해주세요",
-    });
+    msg("error", "모든 정보를 입력해주세요");
     return false;
   }
 
@@ -30,10 +30,7 @@ export const validate = ({ Email, Name, Password, ConfirmPassword }: Form) => {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const isValidEmail = emailPattern.test(Email);
   if (!isValidEmail) {
-    Swal.fire({
-      icon: "error",
-      title: "올바른 이메일의 형태로 입력해주세요",
-    });
+    msg("error", "올바른 이메일의 형태로 입력해주세요");
     return false;
   }
 
@@ -41,21 +38,26 @@ export const validate = ({ Email, Name, Password, ConfirmPassword }: Form) => {
   const namePattern = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;
   const isValidName = namePattern.test(Name);
   if (isValidName) {
-    Swal.fire({
-      icon: "error",
-      title: "이름엔 특수문자가 들어갈 수 없어요",
-    });
+    msg("error", "이름엔 특수문자가 들어갈 수 없어요");
     return false;
   }
 
   return true;
 };
 
-// 에러 발생 시 
-export const onError = () => {
-  Swal.fire({
-    icon: "error",
-    title: "오류가 발생했어요",
-  });
-  return false;
+// 회원가입 검증 및 완료 처리
+export const signupValidate = (result: Action) => {
+  const { payload } = result;
+  
+  if (payload && payload.isSuccess) {
+    msg("success", "회원가입이 완료되었어요");
+    return true;
+  } else if (payload.code && payload.code == 11000) {
+    // unique 위반 / 아이디 중복
+    msg("error", "이미 존재하는 이메일이에요");
+    return false;
+  } else {
+    msg("error", "오류가 발생했어요");
+    return false;
+  }
 };
