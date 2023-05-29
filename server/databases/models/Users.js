e5a8a062d446790bd30c6609cb80db1e0b337c1d
slowteady <vbn0213@gmail.com>
2023-05-29 12:47:23 +0900

    user 스키마 추가

---
+++ server/databases/models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  name: {
+    type: String,
+    maxlength: 10,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
