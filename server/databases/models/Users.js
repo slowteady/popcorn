cde80f45db206b513427f4e7f42e2e291d8cd6a7
slowteady <vbn0213@gmail.com>
2023-05-30 23:12:07 +0900

    bcrypt 이용하여 비밀번호 암호화

--- server/databases/models/Users.js
+++ server/databases/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const brcypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -20,6 +21,28 @@ const userSchema = new Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// 회원가입 이전 이벤트
+userSchema.pre("save", function (next) {
+  const user = this;
+  const saltRounds = 10;
+
+  // 비밀번호 필드에 변화가 있을 때를 감지
+  if (user.isModified("password")) {
+    try {
+      // 비밀번호 암호화
+      brcypt.genSalt(saltRounds, async (err, salt) => {
+        const hash = await brcypt.hash(user.password, salt);
+        user.password = hash;
+        next();
+      });
+    } catch (err) {
+      return next(err);
+    }
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
