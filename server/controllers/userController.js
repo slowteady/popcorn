4db4aa8500b87b5103b2a3b547e4941a7d4b0966
slowteady <vbn0213@gmail.com>
2023-05-31 22:46:58 +0900

    로그인 비즈니스 로직 추가

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,8 +12,23 @@ const registerUser = async (req, res) => {
   }
 };
 
-const loginUser = (req, res) => {
-  
+// 로그인
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.json({ isSuccess: false, msg: "일치하는 사용자가 없어요" });
+    }
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.json({ isSuccess: false, msg: "비밀번호가 일치하지 않아요" });
+    }
+    res.status(200).json({ isSuccess: true });
+  } catch (err) {
+    console.error("err: ", err, "code: ", err.code);
+    res.json({ isSuccess: false, msg: "오류가 발생했어요" });
+  }
 };
 
 module.exports = {
