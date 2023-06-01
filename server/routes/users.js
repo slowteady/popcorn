a0463a1af7bb0225cf0f64e98ef401bdb8ff81e4
slowteady <vbn0213@gmail.com>
2023-06-01 22:25:50 +0900

    로그아웃 라우팅 추가

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,15 @@
 // 로그인, 회원가입 등 유저 관련 라우터
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
-
+const { registerUser, loginUser, logoutUser } = require("../controllers/userController");
 const router = express.Router();
 
 // 회원가입
 router.post("/register", registerUser);
+
 // 로그인
 router.post("/login", loginUser);
+
+// 로그아웃
+router.post("/logout", logoutUser);
 
 module.exports = router;
