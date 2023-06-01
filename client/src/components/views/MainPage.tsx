f5c6d4d1774eb0726e71ca654405e50a75403815
slowteady <vbn0213@gmail.com>
2023-06-01 23:01:27 +0900

    메인페이지, 로그아웃 버튼 임시 생성

--- client/src/components/views/MainPage.tsx
+++ client/src/components/views/MainPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { logoutUser } from "../../services/userService";
+import { loginAndOutValidate } from "../auth/userAuth";
 
+// 메인 페이지
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const history = useHistory();
+  const onclickLogout = async () => {
+    const logout = await logoutUser();
+    loginAndOutValidate(logout);
+    history.push("/");
+  }
+  return <button onClick={onclickLogout}>logout</button>;
 };
 
 export default MainPage;
