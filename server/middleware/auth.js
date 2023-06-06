b041c58153596ec3f9b4b1a89dcd5e275f3b7872
slowteady <vbn0213@gmail.com>
2023-06-06 23:12:37 +0900

    토큰 받는 프로퍼티 변경

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,10 +2,10 @@ const User = require("../databases/models/Users");
 
 // 사용자 검증 미들웨어
 const auth = async (req, res, next) => {
-  let token = req.cookies.x_auth;
+  let token = req.cookies.AUTH_TOKEN;
   try {
     const user = await User.findByToken(token);
-
+    
     req.token = token;
     req.user = user;
     next();
