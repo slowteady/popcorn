0dea769e2c8fd5e4182c1786e5a10ae4ce0925cc
slowteady <vbn0213@gmail.com>
2023-06-06 23:12:06 +0900

    타입 추가

--- client/src/utils/validationUtils.ts
+++ client/src/utils/validationUtils.ts
@@ -1,10 +1,10 @@
 export const strCheck = {
   // 문자 있는지 검증
-  isEmpty: (str: string) => {
+  isEmpty: (str: string | boolean) => {
     return str == "" || str == undefined || str == null || str == "null";
   },
   // 문자 없는지 검증
-  isNotEmpty: (str: string) => {
+  isNotEmpty: (str: string | boolean) => {
     return !strCheck.isEmpty(str);
   },
 }
