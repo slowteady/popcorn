7985ee22e15e691353a38fff67165b892526bcce
slowteady <vbn0213@gmail.com>
2023-06-07 23:57:05 +0900

    alert 창 제거

--- client/src/hoc/Auth.tsx
+++ client/src/hoc/Auth.tsx
@@ -2,7 +2,6 @@ import React, { ComponentType, FunctionComponent, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { auth } from "../services/userService";
 import { removeCookie } from "../utils/cookieUtils";
-import { msg } from "../utils/msgUtils";
 
 const Auth = <P extends {}>(
   SpecificComponent: ComponentType<P>
@@ -10,17 +9,13 @@ const Auth = <P extends {}>(
   const AuthCheck: FunctionComponent<P> = (props) => {
     const history = useHistory();
 
-    const fail = () => {
-      msg("error", "사용자 검증에 실패했어요 다시 로그인 해주세요");
-      history.push("/");
-    };
     useEffect(() => {
       auth()
         .then((response) => {
           const isSuccess = response.payload.isSuccess;
           const isUser = response.payload.user;
           const isExpire = response.payload.isExpire;
-          
+
           // 토큰 만료 시
           if (isExpire) {
             removeCookie("AUTH_TOKEN");
@@ -30,11 +25,11 @@ const Auth = <P extends {}>(
             });
           } else if (!isSuccess || !isUser) {
             // 사용자 검증 실패 시
-            fail();
+            history.push("/");
           }
         })
         .catch((err) => {
-          fail();
+          history.push("/");
         });
     }, []);
 
