534d6f0b0e5b7daf8a809f987cc086bd5dcf60ac
slowteady <vbn0213@gmail.com>
2023-06-07 23:43:17 +0900

    동등 연산자에서 일치 연산자로 변경

--- client/src/utils/validationUtils.ts
+++ client/src/utils/validationUtils.ts
@@ -1,7 +1,7 @@
 export const strCheck = {
   // 문자 있는지 검증
   isEmpty: (str: string | boolean) => {
-    return str == "" || str == undefined || str == null || str == "null";
+    return str === "" || str === undefined || str === null || str === "null";
   },
   // 문자 없는지 검증
   isNotEmpty: (str: string | boolean) => {
