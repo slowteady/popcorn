d242fd64366fdf436ecfd9314048e37e0b119491
LeeYongMin01 <vbn0213@gmail.com>
2023-06-08 16:08:33 +0900

    임시적으로 404 페이지 라우팅 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from "./components/views/LoginPage";
 import MainPage from "./components/views/MainPage";
 import SignupPage from "./components/views/SignupPage";
 import Auth from "./hoc/Auth";
+import Page404 from "./components/views/Page404";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/main" component={Auth(MainPage)} />
+        <Route exact path="/main" component={MainPage} />
+        <Route exact path="/404" component={Page404} />
       </Switch>
     </BrowserRouter>
   );
