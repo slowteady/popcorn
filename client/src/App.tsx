1916ad6936a6ee61709cae7714812110fd968fac
LeeYongMin01 <vbn0213@gmail.com>
2023-06-09 08:57:00 +0900

    HelmetProvider 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,19 @@ import LoginPage from "./components/views/LoginPage";
 import MainPage from "./components/views/MainPage";
 import Page404 from "./components/views/Page404";
 import SignupPage from "./components/views/SignupPage";
+import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/main" component={MainPage} />
-        <Route exact path="/404" component={Page404} />
-      </Switch>
+      <HelmetProvider>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/main" component={MainPage} />
+          <Route exact path="/404" component={Page404} />
+        </Switch>
+      </HelmetProvider>
     </BrowserRouter>
   );
 };
