a41df37dac0221ef953a19233580594f12299c0d
slowteady <vbn0213@gmail.com>
2023-06-09 23:56:23 +0900

    메인 레이아웃 컴포넌트 구현중

---
+++ client/src/components/layouts/MainLayout.tsx
@@ -0,0 +1,43 @@
+import { styled } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./header/Header";
+
+// -------------------------------------------------------------
+
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 92;
+
+const StyledRoot = styled("div")({
+  display: "flex",
+  minHeight: "100%",
+  overflow: "hidden",
+});
+
+const Main = styled("div")(({ theme }) => ({
+  flexGrow: 1,
+  overflow: "auto",
+  minHeight: "100%",
+  paddingTop: APP_BAR_MOBILE + 24,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up("lg")]: {
+    paddingTop: APP_BAR_DESKTOP + 24,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
+
+// -------------------------------------------------------------
+
+const MainLayout = () => {
+  return (
+    <StyledRoot>
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledRoot>
+  );
+};
+
+export default MainLayout;
