1e1bc9644e9c24b20643db218c40e87d87faec28
LeeYongMin01 <vbn0213@gmail.com>
2023-06-09 14:34:15 +0900

    로고 헤더 width 100 제거

--- client/src/layouts/FrameLayout.tsx
+++ client/src/layouts/FrameLayout.tsx
@@ -9,7 +9,6 @@ const StyledHeader = styled("header")(({ theme }) => ({
   top: 0,
   left: 0,
   lineHeight: 0,
-  width: "100%",
   position: "absolute",
   padding: theme.spacing(3, 3, 0),
   [theme.breakpoints.up("sm")]: {
