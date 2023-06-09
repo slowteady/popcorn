96dadfcd6fb46816a87b01d290ed9ae59ce35b93
LeeYongMin01 <vbn0213@gmail.com>
2023-06-09 14:30:40 +0900

    로고 컴포넌트 추가

---
+++ client/src/components/views/Logo/Logo.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Link } from "@mui/material";
+import { SxProps } from "@mui/system";
+import logoImg from "../../img/popcorn_icon.png";
+
+interface LogoProps {
+  sx?: SxProps;
+}
+
+const Logo: FunctionComponent<LogoProps> = ({ sx }) => {
+  const logo = (
+    <Box
+      component="img"
+      src={logoImg}
+      sx={{
+        width: 50,
+        height: 50,
+        cursor: "pointer",
+        ...sx,
+      }}
+    />
+  );
+
+  return (
+    <Link to="/" component={RouterLink} sx={{ display: "contents" }}>
+      {logo}
+    </Link>
+  );
+};
+
+export default Logo;
