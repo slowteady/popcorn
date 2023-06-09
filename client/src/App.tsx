3ba6037826729641ea983c3e673ce07e02fd2065
LeeYongMin01 <vbn0213@gmail.com>
2023-06-09 09:38:20 +0900

    react-router-dom v6에 맞게 라우터 로직 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/views/LoginPage";
-import MainPage from "./components/views/MainPage";
-import Page404 from "./components/views/Page404";
-import SignupPage from "./components/views/SignupPage";
 import { HelmetProvider } from "react-helmet-async";
+import Router from "./Router";
 
 const App = () => {
   return (
     <BrowserRouter>
       <HelmetProvider>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/404" element={<Page404 />} />
-        </Routes>
+        <Router />
       </HelmetProvider>
     </BrowserRouter>
   );
