67e14a9b4d758cc8d391d49cc4dffa41eb36f414
slowteady <vbn0213@gmail.com>
2023-06-10 22:43:47 +0900

    로직 이동

--- client/src/components/views/MainPage.tsx
+++ client/src/components/views/MainPage.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import { useNavigate } from "react-router-dom";
-import { logoutUser } from "../../services/userService";
-import { loginAndOutValidate } from "../auth/userValidate";
 
 // ----------------------------------------------------------------------
 // 메인 페이지
 // ----------------------------------------------------------------------
 
 const MainPage = () => {
-  const navigate = useNavigate();
-  const onclickLogout = async () => {
-    const logout = await logoutUser();
-    loginAndOutValidate(logout);
-    navigate("/");
-  };
+
   return (
     <>
       <Helmet>
         <title> Main | POPCORN! </title>
       </Helmet>
-      <button onClick={onclickLogout}>logout</button>
     </>
   );
 };
