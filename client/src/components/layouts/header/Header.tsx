98d08d71878c2965326906c994819cf2ea3dc5ba
slowteady <vbn0213@gmail.com>
2023-06-10 22:42:51 +0900

    헤더 추가

--- client/src/components/layouts/header/Header.tsx
+++ client/src/components/layouts/header/Header.tsx
@@ -1,7 +1,55 @@
+import { AppBar, Box, Stack, Toolbar } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import React from "react";
+import { bgBlur } from "../../../utils/cssStyles";
+import UserPopover from "./UserPopover";
+
+// ----------------------------------------------------------------------
+// 헤더
+// ----------------------------------------------------------------------
+
+const NAV_WIDTH = 280;
+
+const HEADER_MOBILE = 64;
+
+const HEADER_DESKTOP = 92;
+
+const StyledRoot = styled(AppBar)(({ theme }) => ({
+  ...bgBlur({ color: theme.palette.background.default }),
+  boxShadow: "none",
+  [theme.breakpoints.up("lg")]: {
+    width: `calc(100% - ${NAV_WIDTH + 1}px)`,
+  },
+}));
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  minHeight: HEADER_MOBILE,
+  [theme.breakpoints.up("lg")]: {
+    minHeight: HEADER_DESKTOP,
+    padding: theme.spacing(0, 5),
+  },
+}));
+
+// ----------------------------------------------------------------------
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <StyledRoot>
+      <StyledToolbar>
+        <Box sx={{ flexGrow: 1 }} />
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={{
+            xs: 0.5,
+            sm: 1,
+          }}
+        >
+          <UserPopover />
+        </Stack>
+      </StyledToolbar>
+    </StyledRoot>
+  );
 };
 
 export default Header;
