bc728185ac7bbe5ccf8a70a1272fd4ed88c85134
slowteady <vbn0213@gmail.com>
2023-06-11 12:27:38 +0900

    반응형 구현을 위한 hooks 추가

---
+++ client/src/hooks/useResponsive.tsx
@@ -0,0 +1,53 @@
+import { Breakpoint, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+// ----------------------------------------------------------------------
+// 반응형 구현을 위한 hooks
+// ----------------------------------------------------------------------
+
+interface useResponsiveProps {
+  query: "up" | "down" | "between";
+  start: Breakpoint;
+  end?: number | Breakpoint;
+}
+
+export const useResponsive = ({ query, start, end }: useResponsiveProps) => {
+  const theme = useTheme();
+
+  const mediaUp = useMediaQuery(theme.breakpoints.up(start));
+  const mediaDown = useMediaQuery(theme.breakpoints.down(start));
+  let mediaBetween = false;
+
+  const mediaOnly = useMediaQuery(theme.breakpoints.only(start));
+
+  if (query === "up") {
+    return mediaUp;
+  }
+
+  if (query === "down") {
+    return mediaDown;
+  }
+
+  if (query === "between" && end) {
+    mediaBetween = useMediaQuery(theme.breakpoints.between(start, end));
+    return mediaBetween;
+  }
+
+  return mediaOnly;
+};
+
+// ----------------------------------------------------------------------
+
+export function useWidth() {
+  const theme = useTheme();
+
+  const keys = [...theme.breakpoints.keys].reverse();
+
+  return (
+    keys.reduce<Breakpoint | null>((output, key) => {
+      const matches = useMediaQuery(theme.breakpoints.up(key));
+
+      return !output && matches ? key : output;
+    }, null) || "xs"
+  );
+}
