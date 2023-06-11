f50c6bb0dab473b613b790068857505e409402a7
slowteady <vbn0213@gmail.com>
2023-06-11 12:27:56 +0900

    참조 패키지 변경

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { styled } from "@mui/material/styles";
+import { styled } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
