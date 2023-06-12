b4df2181b2010e2057152fa91323f2186f68ea22
slowteady <vbn0213@gmail.com>
2023-06-12 22:50:06 +0900

    프로파일 수정 페이지, 구현 진행중

--- client/src/components/pages/users/ProfilePage.tsx
+++ client/src/components/pages/users/ProfilePage.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import { Avatar, Box, Container, FormControl, Typography } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { mock } from "../../../state/_mock/mock";
+
+// ----------------------------------------------------------------------
+// 프로파일 수정 페이지
+// ----------------------------------------------------------------------
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [isHovered, setIsHovered] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleImgBox = () => {
+    if(fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    console.log(file);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title> Profile | POPCORN! </title>
+      </Helmet>
+
+      <Container component="main" maxWidth="sm">
+        <Box
+          sx={{
+            mt: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{ position: "relative" }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Avatar
+              src={mock.photoURL}
+              alt="photoURL"
+              sx={{ width: 72, height: 72 }}
+            />
+            {isHovered && (
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  bgcolor: "rgba(0, 0, 0, 0.5)",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                onClick={handleImgBox}
+              >
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                  style={{ display: "none" }}
+                />
+                <Typography color="white" fontSize={14}>
+                  이미지 추가
+                </Typography>
+              </Box>
+            )}
+          </Box>
+          <FormControl></FormControl>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default ProfilePage;
