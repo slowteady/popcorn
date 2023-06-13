4ab64b8dbf16c1fd4c3085054b3154b508e7fa6b
LeeYongMin01 <vbn0213@gmail.com>
2023-06-13 14:09:20 +0900

    프로파일 폼 컴포넌트 추가

---
+++ client/src/components/utils/ProfileForm.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { FormEvent, useState } from "react";
+import { mock } from "../../state/_mock/mock";
+import { updateProfile } from "../../services/userService";
+import { ProfileFormProps } from "../../types/users/userTypes";
+
+// ----------------------------------------------------------------------
+// 프로파일 폼 처리
+// ----------------------------------------------------------------------
+
+const ProfileForm = ({ avatarImg }: ProfileFormProps) => {
+  const [selfIntro, setSelfIntro] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = { selfIntro, avatarImg };
+
+    const updateResult = await updateProfile(data);
+  };
+  return (
+    <>
+      <Box
+        sx={{
+          mt: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "80%",
+        }}
+        component="form"
+        onSubmit={handleSubmit}
+      >
+        <Typography color="black" fontSize={18}>
+          {mock.displayName}
+        </Typography>
+        <Typography color="black" fontSize={18}>
+          {mock.email}
+        </Typography>
+        <TextField
+          label="자기소개"
+          value={selfIntro}
+          onChange={(e) => setSelfIntro(e.target.value)}
+          inputProps={{ maxLength: 100 }}
+          multiline
+          rows={5}
+          sx={{ mt: 2, width: "100%" }}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          저장
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default ProfileForm;
