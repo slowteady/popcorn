18bebb185aeb07e85ca839462b4f0f7ef812b202
slowteady <vbn0213@gmail.com>
2023-06-13 22:32:38 +0900

    파일 크기 50mb로 제한

--- server/index.js
+++ server/index.js
@@ -10,20 +10,22 @@ const mongoose = require("mongoose");
 const connect = mongoose.connect(process.env.MONGO_URI);
 connect
   .then((res) =>
-    console.log(`========MongoDB Connect To [${res.connections[0].name}]========`)
+    console.log(
+      `========MongoDB Connect To [${res.connections[0].name}]========`
+    )
   )
   .catch((err) => console.error(err));
 
 // Body 파싱 미들웨어 등록
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Cookie 미들웨어 등록
 app.use(cookieParser());
 
 // 유저 라우터
-app.use('/api/users', require('./routes/users'));
+app.use("/api/users", require("./routes/users"));
 
 // 포트번호 설정
 const port = process.env.SERVER_PORT || 8080;
