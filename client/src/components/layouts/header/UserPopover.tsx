1cde0563a00e7c0e3f1c08d220f78b3538bd308b
slowteady <vbn0213@gmail.com>
2023-06-17 00:01:26 +0900

    헤더 유저 mock 데이터 제거

--- client/src/components/layouts/header/UserPopover.tsx
+++ client/src/components/layouts/header/UserPopover.tsx
@@ -68,7 +68,7 @@ const UserPopover = () => {
           }),
         }}
       >
-        <Avatar src={mock.photoURL} alt="photoURL" />
+        <Avatar />
       </IconButton>
 
       {element && (
