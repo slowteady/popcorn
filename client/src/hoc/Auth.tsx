5aff23ec89ed85ebcb9636636bdfbd619256c0ed
slowteady <vbn0213@gmail.com>
2023-06-18 15:04:30 +0900

    사용자 검증 컴포넌트 로직 변경

--- client/src/hoc/Auth.tsx
+++ client/src/hoc/Auth.tsx
@@ -1,46 +1,43 @@
-import React, { ComponentType, FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, ReactNode, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../services/userService";
 import { removeCookie } from "../utils/cookieUtils";
 
 // ----------------------------------------------------------------------
-// 사용자 검증 로직
+// 사용자 검증 컴포넌트
 // ----------------------------------------------------------------------
 
-const Auth = <P extends {}>(
-  SpecificComponent: ComponentType<P>
-): FunctionComponent<P> => {
-  const AuthCheck: FunctionComponent<P> = (props) => {
-    const navigate = useNavigate();
+interface AuthProps {
+  children: ReactNode;
+}
+const Auth: FunctionComponent<AuthProps> = ({ children }) => {
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      auth()
-        .then((response) => {
-          const isSuccess = response.payload.isSuccess;
-          const isUser = response.payload.user;
-          const isExpire = response.payload.isExpire;
+  useEffect(() => {
+    auth()
+      .then((response) => {
+        const isSuccess = response.payload.isSuccess;
+        const isUser = response.payload.user;
+        const isExpire = response.payload.isExpire;
 
-          // 토큰 만료 시
-          if (isExpire) {
-            removeCookie("AUTH_TOKEN");
-            navigate({
-              pathname: "/",
-              search: `?expired=true`,
-            });
-          } else if (!isSuccess || !isUser) {
-            // 사용자 검증 실패 시
-            navigate("/");
-          }
-        })
-        .catch((err) => {
+        // 토큰 만료 시
+        if (isExpire) {
+          removeCookie("AUTH_TOKEN");
+          navigate({
+            pathname: "/",
+            search: `?expired=true`,
+          });
+        } else if (!isSuccess || !isUser) {
+          // 사용자 검증 실패 시
           navigate("/");
-        });
-    }, []);
+        }
+      })
+      .catch((err) => {
+        navigate("/");
+      });
+  }, []);
 
-    return <SpecificComponent {...props} />;
-  };
-
-  return AuthCheck;
+  return <>{children}</>;
 };
 
 export default Auth;
