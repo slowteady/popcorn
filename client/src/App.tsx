c236b4801c509c39d57a8c944359f3c5d8819438
slowteady <vbn0213@gmail.com>
2023-06-21 21:59:46 +0900

    API 요청 다시 하지 않도록 디폴트 프로퍼티 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,14 @@ import Router from "./Router";
 import ThemeProvider from "./theme";
 
 // react-query 사용을 위한 객체 생성
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 앱이 다시 포커싱 받을 시 재요청 할지 여부
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return (
