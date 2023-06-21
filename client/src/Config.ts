cf9988938277658309364c5b8f3ac8348e4c3196
slowteady <vbn0213@gmail.com>
2023-06-21 21:02:28 +0900

    API 요청을 위한 기본 Config값 추가

---
+++ client/src/Config.ts
@@ -0,0 +1,9 @@
+//ex) https://api.themoviedb.org/3/movie/popular?api_key=50bce08bd7644cf283922a7b984a50ee&language=ko-KR
+
+export const API = {
+  API_KEY: "50bce08bd7644cf283922a7b984a50ee",
+  LANGUAGE: "ko-KR",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  IMAGE_BASE_URL: "http://image.tmdb.org/t/p/",
+  POPULAR_PATH: "movie/popular",
+};
