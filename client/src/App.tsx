8bda2fafd987f7fade9f07b908c6a4797e8b766e
slowteady <vbn0213@gmail.com>
2023-06-21 21:16:54 +0900

    react-query hoc 씌우는 로직 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Router from "./Router";
 import ThemeProvider from "./theme";
+
+// react-query 사용을 위한 객체 생성
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <HelmetProvider>
       <BrowserRouter>
         <ThemeProvider>
-          <RecoilRoot>
-            <Router />
-          </RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+              <Router />
+            </RecoilRoot>
+          </QueryClientProvider>
         </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
