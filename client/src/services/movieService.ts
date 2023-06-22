edc16df19c2ded3e9e67150de00ef8783876211b
slowteady <vbn0213@gmail.com>
2023-06-22 23:36:18 +0900

    API 요청 서비스 메소드 추가

--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -1,3 +1,22 @@
-export const getPopularMovies = () => {
+import axios from "axios";
+import { API } from "../Config";
 
-}
+// popular movie api 요청
+export const getPopularMovies = async (url: string) => {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        api_key: API.API_KEY,
+        language: API.LANGUAGE,
+      },
+    });
+
+    const obj = { isSuccess: true, payload: response.data.results };
+    return obj;
+  } catch (err) {
+    console.error(err);
+    return {
+      payload: { isSuccess: false, msg: "오류가 발생했어요" },
+    };
+  }
+};
