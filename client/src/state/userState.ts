a2722416ae96549d3f636f564163a90bf161ad8d
LeeYongMin01 <vbn0213@gmail.com>
2023-06-22 16:02:28 +0900

    영화 API 호출 type 전역 state 추가

--- client/src/state/userState.ts
+++ client/src/state/userState.ts
@@ -11,3 +11,8 @@ export const userData = atom<userDataType | string>({
   key: "userData",
   default: "",
 });
+
+export const movieListType = atom({
+  key: "movieListType",
+  default: { value: "POPULAR", label: "Popular" },
+});
