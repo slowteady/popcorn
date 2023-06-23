a1cb5d213fd58b9b8462c34122b7f7e6f1e99e7b
slowteady <vbn0213@gmail.com>
2023-06-24 00:33:30 +0900

    Movie 리스트 페이지 구현

--- client/src/components/pages/main/movies/MovieList.tsx
+++ client/src/components/pages/main/movies/MovieList.tsx
@@ -1,7 +1,22 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import { MovieListProps } from "../../../../types/movies/movieTypes";
+import MovieCard from "./MovieCard";
 
-const MovieList = () => {
-  return <div>MovieList</div>;
+// ----------------------------------------------------------------------
+// Movie 리스트 페이지
+// ----------------------------------------------------------------------
+
+const MovieList = ({ movies }: MovieListProps) => {
+  return (
+    <Grid container spacing={3}>
+      {movies.map((movie) => (
+        <Grid key={movie.id} item xs={12} sm={6} md={3}>
+          <MovieCard movie={movie} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default MovieList;
