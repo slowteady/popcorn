8e1b97d4b9312c753b79a463fefa8b03e8cfda25
LeeYongMin01 <vbn0213@gmail.com>
2023-06-23 17:00:26 +0900

    영화 페이지 구현 진행중

--- client/src/components/pages/main/movies/MoviesPage.tsx
+++ client/src/components/pages/main/movies/MoviesPage.tsx
@@ -7,12 +7,14 @@ import { getPopularMovies } from "../../../../services/movieService";
 import { movieListType } from "../../../../state/movieState";
 import MovieList from "./MovieList";
 import MovieType from "./MovieType";
+import { useQuery } from "react-query";
 
 // ----------------------------------------------------------------------
 // Movies 페이지
 // ----------------------------------------------------------------------
 
 const MoviesPage = () => {
+  console.log(1);
   const type = useRecoilValue(movieListType);
 
   let url = API.BASE_URL;
@@ -29,14 +31,21 @@ const MoviesPage = () => {
     default:
       url += API.POPULAR_PATH;
   }
-  
-  useEffect(() => {
-    const movies = async () => {
-      // 서비스 로직에 url 전달
-      const result = await getPopularMovies(url);
-    };
-    movies();
-  }, [type]);
+
+  // id, poster_path, release_date, title, genre_ids
+  const { data } = useQuery(["movieData"], () => getPopularMovies(url));
+  if (data && data.payload.isSuccess) {
+    const { id, poster_path, release_date, title, genre_ids } = data.payload;
+    console.log(id, poster_path, release_date, title, genre_ids);
+  }
+
+  // useEffect(() => {
+  //   const movies = async () => {
+  //     // 서비스 로직에 url 전달
+  //     const result = await getPopularMovies(url);
+  //   };
+  //   movies();
+  // }, [type]);
 
   return (
     <>
