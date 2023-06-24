95f5023b71caddc282605fc276f0597cf34351af
slowteady <vbn0213@gmail.com>
2023-06-24 23:08:24 +0900

    MovieCard 컴포넌트 추가

--- client/src/components/pages/main/movies/MovieCard.tsx
+++ client/src/components/pages/main/movies/MovieCard.tsx
@@ -1,5 +1,6 @@
-import { styled } from "@mui/material";
+import { Box, Card, Link, Stack, Typography, styled } from "@mui/material";
 import React from "react";
+import { API } from "../../../../Config";
 import { MovieCardProps } from "../../../../types/movies/movieTypes";
 
 // ----------------------------------------------------------------------
@@ -16,8 +17,30 @@ const StyledMovieImg = styled("img")({
 
 const MovieCard = ({ movie }: MovieCardProps) => {
   const { id, poster_path, release_date, title, genre_ids } = movie;
-  
-  return <div>MovieCard</div>;
+  const posterUrl = `${API.IMAGE_BASE_URL}${API.IMAGE_SIZE_300}${poster_path}`;
+
+  return (
+    <Card>
+      <Box sx={{ minHeight: "300px", position: "relative" }}>
+        {poster_path && <StyledMovieImg alt={title} src={posterUrl} />}
+      </Box>
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Link color="inherit" underline="hover">
+          <Typography component="div" title={title} variant="subtitle2" noWrap>
+            {title}
+          </Typography>
+        </Link>
+
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="subtitle1">{release_date}</Typography>
+        </Stack>
+      </Stack>
+    </Card>
+  );
 };
 
 export default MovieCard;
