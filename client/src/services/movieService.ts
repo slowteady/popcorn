dfa4739a780c17de1ae3d068519b1b37d57cde6d
slowteady <vbn0213@gmail.com>
2023-06-29 00:02:26 +0900

    API 요청을 위한 서비스 추가

--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -21,3 +21,44 @@ export const getMovieData = async (url: string, page: number) => {
     };
   }
 };
+
+// movie api 디테일 요청
+export const getMovieDetailData = async (url: string) => {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        api_key: API.API_KEY,
+        language: API.LANGUAGE,
+      },
+    });
+
+    const {
+      genres,
+      overview,
+      poster_path,
+      release_date,
+      runtime,
+      title,
+      vote_average,
+    } = response.data;
+
+    const obj = {
+      isSuccess: true,
+      payload: {
+        genres,
+        overview,
+        poster_path,
+        release_date,
+        runtime,
+        title,
+        vote_average,
+      },
+    };
+    return obj;
+  } catch (err) {
+    console.error(err);
+    return {
+      payload: { isSuccess: false, msg: "오류가 발생했어요" },
+    };
+  }
+};
