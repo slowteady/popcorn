33ffd5f7b5636fbeb7b6be59ddaf644b39a3b075
LeeYongMin01 <vbn0213@gmail.com>
2023-06-29 19:01:26 +0900

    Grid 이용하여 레이아웃 제작중

--- client/src/components/pages/main/movies/ModalPage.tsx
+++ client/src/components/pages/main/movies/ModalPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { API } from "../../../../Config";
 import { getMovieDetailData } from "../../../../services/movieService";
+import { Grid } from "@mui/material";
 
 // ----------------------------------------------------------------------
 // 모달 페이지
@@ -13,13 +14,11 @@ interface ModalPageProps {
 
 const ModalPage = ({ id }: ModalPageProps) => {
   const [movie, setMovie] = useState({});
-  const [enabled, setEnabled] = useState(false);
 
   const url = `${API.BASE_URL}movie/${id}`;
   const { status, data } = useQuery(
     ["movieDetailData", url],
     () => getMovieDetailData(url),
-    { enabled: true }
   );
 
   useEffect(() => {
@@ -28,7 +27,11 @@ const ModalPage = ({ id }: ModalPageProps) => {
     }
   }, [data]);
 
-  return <></>;
+  return (
+    <>
+      <Grid container spacing={1}></Grid>
+    </>
+  );
 };
 
 export default ModalPage;
