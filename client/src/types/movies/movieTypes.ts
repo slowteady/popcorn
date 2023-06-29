df06e610a0f35beaf366f8ecb865da54e2daf881
slowteady <vbn0213@gmail.com>
2023-06-30 00:53:45 +0900

    movie 데이터 타입 추가

--- client/src/types/movies/movieTypes.ts
+++ client/src/types/movies/movieTypes.ts
@@ -4,7 +4,7 @@ export interface MovieListProps {
     poster_path: string;
     release_date: string;
     title: string;
-    vote_average: string;
+    vote_average: number;
     genre_ids: number[];
   }[];
 }
@@ -15,7 +15,19 @@ export interface MovieCardProps {
     poster_path: string;
     release_date: string;
     title: string;
-    vote_average: string;
+    vote_average: number;
     genre_ids: number[];
+  };
+}
+
+export interface ModalMovieProps {
+  movie: {
+    title: string;
+    poster_path: string;
+    overview: string;
+    release_date: string;
+    vote_average: number;
+    runtime: number;
+    genres: [{ id: number; name: string }];
   };
 }
