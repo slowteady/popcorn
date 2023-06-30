a9eb55056d1368c95d305a4ec4050029fd8a84cf
LeeYongMin01 <vbn0213@gmail.com>
2023-06-30 12:43:05 +0900

    예시 추가

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -2,6 +2,7 @@
 // 2. 영화 세부조회 https://api.themoviedb.org/3/movie/343611?api_key=50bce08bd7644cf283922a7b984a50ee&language=ko
 // 3. 이미지 https://api.themoviedb.org/3/movie/385687/images?api_key=50bce08bd7644cf283922a7b984a50ee
 // 4. 영화 검색 https://api.themoviedb.org/3/search/movie?api_key=50bce08bd7644cf283922a7b984a50ee&query=harry&language=ko-KR
+// 5. 감독, 배우 검색 https://api.themoviedb.org/3/movie/1010581/credits?api_key=50bce08bd7644cf283922a7b984a50ee&language=ko
 
 export const API = {
   API_KEY: "50bce08bd7644cf283922a7b984a50ee",
