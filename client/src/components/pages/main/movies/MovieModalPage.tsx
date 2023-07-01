99f05de47abe0bca06adecacdd866fb26ba94ddc
slowteady <vbn0213@gmail.com>
2023-07-01 23:14:37 +0900

    state 타입 변경

--- client/src/components/pages/main/movies/MovieModalPage.tsx
+++ client/src/components/pages/main/movies/MovieModalPage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { API } from "../../../../Config";
 import { getMovieDetailData } from "../../../../services/movieService";
-import { ModalMovieProps } from "../../../../types/movies/movieTypes";
+import { MovieDetailProps } from "../../../../types/movies/movieTypes";
 
 // ----------------------------------------------------------------------
 // 모달 페이지
@@ -22,8 +22,7 @@ interface ModalPageProps {
 }
 
 const MovieModalPage = ({ id }: ModalPageProps) => {
-  const [movie, setMovie] = useState<ModalMovieProps["movie"]>();
-
+  const [movie, setMovie] = useState<MovieDetailProps["movie"]>();
   const url = `${API.BASE_URL}movie/${id}`;
   const { status, data } = useQuery(["movieDetailData", url], () =>
     getMovieDetailData(url)
