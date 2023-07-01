6091431c8954e3052925671d6194d5c78de971d2
slowteady <vbn0213@gmail.com>
2023-07-01 23:14:21 +0900

    디테일 페이지 데이터변환 객체 타입 지정

--- client/src/types/movies/movieTypes.ts
+++ client/src/types/movies/movieTypes.ts
@@ -20,14 +20,39 @@ export interface MovieCardProps {
   };
 }
 
-export interface ModalMovieProps {
+export interface MovieModalProps {
   movie: {
     title: string;
     poster_path: string;
     tagline: string;
     release_date: string;
     vote_average: number;
-    runtime: number;
+    runtime: number | string;
     genres: [{ id: number; name: string }];
+  };
+}
+
+export interface MovieCreditsProps {
+  credits: {
+    crew?: {
+      [key: string]: any;
+    };
+    cast?: {
+      [key: string]: any;
+    };
+  };
+}
+
+export interface MovieDetailProps {
+  movie: {
+    genres: string[];
+    tagline: string;
+    poster_path: string;
+    release_date: string;
+    runtime: number | string;
+    title: string;
+    vote_average: number | string;
+    actor: string[];
+    director: string[];
   };
 }
