97b134ad29cd47275106bad64471370485c1bbc4
slowteady <vbn0213@gmail.com>
2023-07-03 21:55:55 +0900

    문자 유효성 검사 util 조건 추가

--- client/src/utils/validationUtils.ts
+++ client/src/utils/validationUtils.ts
@@ -3,11 +3,17 @@
 // ----------------------------------------------------------------------
 export const strCheck = {
   // 문자 있는지 검증
-  isEmpty: (str: string | boolean) => {
-    return str === "" || str === undefined || str === null || str === "null";
+  isEmpty: (str: string) => {
+    return (
+      str === "" ||
+      str === undefined ||
+      str === null ||
+      str === "null" ||
+      str.trim() === ""
+    );
   },
   // 문자 없는지 검증
-  isNotEmpty: (str: string | boolean) => {
+  isNotEmpty: (str: string) => {
     return !strCheck.isEmpty(str);
   },
 };
