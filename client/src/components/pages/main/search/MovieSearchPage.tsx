f1d2f17800bc3d49833c895316e57141b58d66c5
slowteady <vbn0213@gmail.com>
2023-07-03 21:09:10 +0900

    Search 페이지 구현중

--- client/src/components/pages/main/search/MovieSearchPage.tsx
+++ client/src/components/pages/main/search/MovieSearchPage.tsx
@@ -1,7 +1,29 @@
+import { Container, Stack, Typography } from "@mui/material";
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import MovieSearch from "./MovieSearch";
 
 const MovieSearchPage = () => {
-  return <div>MovieSearchPage</div>;
+  return (
+    <>
+      <Helmet>
+        <title> Search | POPCORN! </title>
+      </Helmet>
+      <Container>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Typography variant="h4" gutterBottom>
+            Search
+          </Typography>
+        </Stack>
+      </Container>
+      <MovieSearch />
+    </>
+  );
 };
 
 export default MovieSearchPage;
