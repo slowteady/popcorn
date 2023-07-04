05de2143a84a569068cc1ec44c1819ac5d91e11f
slowteady <vbn0213@gmail.com>
2023-07-04 23:27:19 +0900

    영화 검색 창 구현중

--- client/src/components/pages/main/search/MovieSearch.tsx
+++ client/src/components/pages/main/search/MovieSearch.tsx
@@ -1,18 +1,57 @@
-import { Button, InputAdornment, TextField } from "@mui/material";
-import React from "react";
+import { Button, Container, InputAdornment, TextField } from "@mui/material";
+import React, {
+  ChangeEvent,
+  KeyboardEvent,
+  MouseEvent,
+  useEffect,
+  useRef,
+} from "react";
+import { useLocation } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { searchKeyword } from "../../../../state/searchState";
 import { strCheck } from "../../../../utils/validationUtils";
 import Iconify from "../../../iconify/Iconify";
 
+// ----------------------------------------------------------------------
+// 영화 검색 창
+// ----------------------------------------------------------------------
+
 const MovieSearch = () => {
   const [keyword, setKeyword] = useRecoilState(searchKeyword);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const location = useLocation();
+
+  // 특수문자 방지
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const newValue = value.replace(/[^a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣\s]/gi, "");
+    setKeyword(newValue);
+  };
+
+  useEffect(() => {
+    if (strCheck.isEmpty(location.state?.search)) {
+      setKeyword("");
+    }
+  }, [location.state?.search]);
+
+  // 엔터 키 입력 시
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      setKeyword("");
+    }
+  };
+
+  // 버튼 클릭 시
+  const handleClick = (e: MouseEvent) => {
+    setKeyword("");
+  };
 
   return (
-    <>
+    <Container>
       <TextField
-        value={keyword && strCheck.isNotEmpty(keyword) ? keyword : undefined}
-        sx={{ width: 280, ml: "24px" }}
+        value={keyword}
+        sx={{ width: 280 }}
+        inputRef={inputRef}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
@@ -24,11 +63,13 @@ const MovieSearch = () => {
           ),
         }}
         placeholder="영화를 검색해주세요"
+        onKeyDown={handleKeyDown}
+        onChange={handleInputChange}
       />
-      <Button variant="contained" sx={{ margin: "10px" }}>
+      <Button variant="contained" sx={{ margin: "10px" }} onClick={handleClick}>
         Search
       </Button>
-    </>
+    </Container>
   );
 };
 
