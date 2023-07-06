a0bdcd5ced7f51c5214664099c6405646512d30b
LeeYongMin01 <vbn0213@gmail.com>
2023-07-06 15:09:11 +0900

    전역 state 추가

--- client/src/state/movieState.ts
+++ client/src/state/movieState.ts
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
+import { MovieListProps } from "../types/movies/movieTypes";
 
 export const movieListType = atom({
   key: "movieListType",
   default: { value: "POPULAR", label: "Popular" },
+});
+
+export const moviesSearchList = atom({
+  key: "moviesSearchList",
+  default: [] as MovieListProps["movies"],
 });
