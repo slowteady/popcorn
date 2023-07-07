6d4f0b58c75b77e83124ac06465ad1439988112a
LeeYongMin01 <vbn0213@gmail.com>
2023-07-07 11:31:09 +0900

    무비서치 페이지 진행중

--- client/src/components/pages/main/search/MovieSearch.tsx
+++ client/src/components/pages/main/search/MovieSearch.tsx
@@ -22,85 +22,25 @@ import { moviesSearchList } from "../../../../state/movieState";
 
 const MovieSearch = () => {
   const [keyword, setKeyword] = useRecoilState(searchKeyword);
-  const [movie, setMovie] = useRecoilState(moviesSearchList);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [enabled, setEnabled] = useState(false);
-  const [query, setQuery] = useState("");
-  const [preValue, setPreValue] = useState("");
+  const [inputValue, setInputValue] = useState(keyword);
   const location = useLocation();
-
-  const { status, data } = useQuery(
-    ["searchMovieData", query],
-    () => getSearchMovieData(query),
-    { enabled }
-  );
-
-  // 특수문자 방지
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    const newValue = value.replace(/[^a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣\s]/gi, "");
-    setKeyword(newValue);
-  };
-
-  // search 파라미터 존재하지 않을 시, keyword 초기화
+    
   useEffect(() => {
-    if (strCheck.isEmpty(location.state?.search)) {
+    // 좌측 메뉴를 통해 들어온 경우
+    if (!location.state || !location.state.search) {
       setKeyword("");
-      setQuery("");
-      setMovie([]);
-    } else if (strCheck.isNotEmpty(location.state?.search)) {
-      setQuery(keyword);
-      setEnabled(true);
-      setPreValue(keyword);
     }
-  }, [location.key, location.state?.search]);
+  }, [location]);
 
   useEffect(() => {
-    if (status === "success") {
-      setMovie((prevMovie) => [...prevMovie, ...data.payload]);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    console.log(movie);
-  }, [movie]);
-
-  // API 요청을 위해 필요한 state를 업데이트 하는 함수
-  const onRequest = () => {
-    const input = inputRef.current;
-
-    if (input) {
-      const value = input.value;
-      console.log(value, preValue);
-      if (value !== preValue) {
-        setMovie([]);
-      }
-      setQuery(value);
-      setPreValue(value);
-    }
-    setEnabled(true);
-  };
-
-  // 엔터 키 입력 시
-  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "Enter" && strCheck.isNotEmpty(keyword)) {
-      onRequest();
-    }
-  };
-
-  // 버튼 클릭 시
-  const handleClick = (e: MouseEvent) => {
-    if (strCheck.isNotEmpty(keyword)) {
-      onRequest();
-    }
-  };
+    setInputValue(keyword);
+  }, [keyword]);
 
   return (
     <Container>
       <TextField
-        value={keyword}
         sx={{ width: 280 }}
-        inputRef={inputRef}
+        value={inputValue}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
@@ -112,10 +52,8 @@ const MovieSearch = () => {
           ),
         }}
         placeholder="영화를 검색해주세요"
-        onKeyDown={handleKeyDown}
-        onChange={handleInputChange}
       />
-      <Button variant="contained" sx={{ margin: "10px" }} onClick={handleClick}>
+      <Button variant="contained" sx={{ margin: "10px" }}>
         Search
       </Button>
     </Container>
