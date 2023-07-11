d60d7b37296a937f0202da1e6420d06221221abc
LeeYongMin01 <vbn0213@gmail.com>
2023-07-11 14:43:42 +0900

    MovieList 렌더링할 컴포넌트 조건 분기 처리

--- client/src/components/pages/main/movies/MovieList.tsx
+++ client/src/components/pages/main/movies/MovieList.tsx
@@ -2,13 +2,14 @@ import { Grid } from "@mui/material";
 import React from "react";
 import { MovieListProps } from "../../../../types/movies/movieTypes";
 import MovieCard from "./MovieCard";
+import Nodata from "../../Nodata";
 
 // ----------------------------------------------------------------------
 // Movie 리스트
 // ----------------------------------------------------------------------
 
 const MovieList = ({ movies }: MovieListProps) => {
-  return (
+  return movies && movies.length !== 0 ? (
     <Grid container spacing={3}>
       {movies.map((movie, index) => (
         <Grid key={index} item xs={12} sm={6} md={3}>
@@ -16,6 +17,13 @@ const MovieList = ({ movies }: MovieListProps) => {
         </Grid>
       ))}
     </Grid>
+  ) : (
+    <Nodata
+      msg="검색 결과가 없습니다."
+      icon="material-symbols:info-outline"
+      sx={{ height: "100%", width: "100%", textAlign: "center" }}
+      containerSx={{ mt: 25 }}
+    />
   );
 };
 
