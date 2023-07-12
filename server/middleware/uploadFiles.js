1702d3ffc47333a5465bcf9bb3ad835815ae7d08
LeeYongMin01 <vbn0213@gmail.com>
2023-07-12 15:04:57 +0900

    파일명 중복 방지를 위한 파일명에 date 추가

--- server/middleware/uploadFiles.js
+++ server/middleware/uploadFiles.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     cb(null, repo);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname + '-' + Date.now());
   },
 });
 
