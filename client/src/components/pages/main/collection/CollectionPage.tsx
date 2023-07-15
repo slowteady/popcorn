aa0289ce14a870ab6ba7dff252752490e6675769
LeeYongMin01 <vbn0213@gmail.com>
2023-07-15 16:21:10 +0900

    컬렉션 페이지 추가, movieList 컴포넌트 재사용을 위한 state 전역으로 생성

--- client/src/components/pages/main/collection/CollectionPage.tsx
+++ client/src/components/pages/main/collection/CollectionPage.tsx
@@ -1,9 +1,27 @@
 import { Button, Container, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Iconify from "../../../iconify/Iconify";
+import AddMovieModal from "./AddMovieModal";
+import { useSetRecoilState } from "recoil";
+import { isCollectionPage } from "../../../../state/movieState";
+
+// ----------------------------------------------------------------------
+// Collection 페이지
+// ----------------------------------------------------------------------
 
 const CollectionPage = () => {
+  const [open, setOpen] = useState(false);
+  const setIsCollection = useSetRecoilState(isCollectionPage);
+  const handleModalOpen = () => {
+    setOpen(true);
+    setIsCollection(true);
+  };
+  const handleModalClose = () => {
+    setOpen(false);
+    setIsCollection(false);
+  };
+
   return (
     <>
       <Helmet>
@@ -23,10 +41,12 @@ const CollectionPage = () => {
           <Button
             variant="contained"
             startIcon={<Iconify icon="eva:plus-fill" />}
+            onClick={handleModalOpen}
           >
             New Collection
           </Button>
         </Stack>
+        <AddMovieModal open={open} handleClose={handleModalClose} />
       </Container>
     </>
   );
