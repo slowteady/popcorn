185c389c28192e75704f3ddeedc671a45df4efd8
LeeYongMin01 <vbn0213@gmail.com>
2023-07-15 13:44:23 +0900

    Collection 페이지 헤더 추가, new 버튼 추가

--- client/src/components/pages/main/collection/CollectionPage.tsx
+++ client/src/components/pages/main/collection/CollectionPage.tsx
@@ -1,7 +1,35 @@
+import { Button, Container, Stack, Typography } from "@mui/material";
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import Iconify from "../../../iconify/Iconify";
 
 const CollectionPage = () => {
-  return <div>CollectionPage</div>;
+  return (
+    <>
+      <Helmet>
+        <title> Collection | POPCORN! </title>
+      </Helmet>
+
+      <Container>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Typography variant="h4" gutterBottom>
+            Collection
+          </Typography>
+          <Button
+            variant="contained"
+            startIcon={<Iconify icon="eva:plus-fill" />}
+          >
+            New Collection
+          </Button>
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default CollectionPage;
