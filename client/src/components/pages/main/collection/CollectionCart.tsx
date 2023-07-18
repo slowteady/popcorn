0f5ef903d2c8813beda9ad60c81ed916fc38ff83
slowteady <vbn0213@gmail.com>
2023-07-18 16:33:14 +0900

    컬렉션 카트 컴포넌트 구현 진행중

--- client/src/components/pages/main/collection/CollectionCart.tsx
+++ client/src/components/pages/main/collection/CollectionCart.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Container, TextField } from "@mui/material";
+import React from "react";
 
 const CollectionCart = () => {
-  return <div>CollectionCart</div>;
+  return (
+    <Container sx={{ mt: 4, ml: 2 }}>
+      <TextField
+        required
+        fullWidth
+        size="small"
+        sx={{ mr: 1, mb: 2 }}
+      ></TextField>
+    </Container>
+  );
 };
 
 export default CollectionCart;
