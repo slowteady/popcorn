64d966f9c8f95cb548962493a7bfd484cfeab229
slowteady <vbn0213@gmail.com>
2023-07-18 15:07:34 +0900

    컴포넌트 사이즈 조절 함수 추가

---
+++ client/src/components/utils/size.ts
@@ -0,0 +1,13 @@
+// 컬렉션 페이지 여부에 따른 사이즈 조절 함수
+export const whichContainerSize = (isCollection: boolean) => {
+  return isCollection
+    ? {
+        containerSize: { spacing: 2 },
+        itemSize: { xs: 8, sm: 7, md: 6, lg: 6 },
+        cartSize: { xs: 4, sm: 5, md: 6, lg: 6 },
+      }
+    : {
+        containerSize: { spacing: 3 },
+        itemSize: { xs: 8, sm: 6, md: 3, lg: 3 },
+      };
+};
