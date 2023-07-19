be5250d027e5fe595c4e8796aaef5f5f3fcc5761
slowteady <vbn0213@gmail.com>
2023-07-19 16:58:23 +0900

    컬렉션 추가 리스트 컴포넌트 구현중

--- client/src/components/pages/main/collection/CollectionCart.tsx
+++ client/src/components/pages/main/collection/CollectionCart.tsx
@@ -1,15 +1,47 @@
-import { Container, TextField } from "@mui/material";
+import {
+  Box,
+  Container,
+  Table,
+  TableBody,
+  TableContainer,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import ListTableHead from "../../../layouts/tables/ListTableHead";
+
+// ----------------------------------------------------------------------
+// 컬렉션 추가 리스트 카트
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: "title", label: "제목", alignRight: false },
+  { id: "release_date", label: "릴리즈", alignRight: false },
+];
 
 const CollectionCart = () => {
+  // submit
+  const handleSubmit = () => {};
+
   return (
     <Container sx={{ mt: 4, ml: 2 }}>
-      <TextField
-        required
-        fullWidth
-        size="small"
-        sx={{ mr: 1, mb: 2 }}
-      ></TextField>
+      <Box onSubmit={handleSubmit}>
+        <Typography color="black" fontSize={15} sx={{ mb: 1 }}>
+          제목
+        </Typography>
+        <TextField required fullWidth size="small" sx={{ mr: 1, mb: 2 }} />
+        <Typography color="black" fontSize={15} sx={{ mb: 1 }}>
+          리스트
+        </Typography>
+        <TableContainer>
+          <Table>
+            <ListTableHead headLabel={TABLE_HEAD} />
+          </Table>
+        </TableContainer>
+        <TableBody>
+          
+        </TableBody>
+      </Box>
     </Container>
   );
 };
