134209b5f73f7937e645b8eab2c996071afe17b2
slowteady <vbn0213@gmail.com>
2023-07-21 16:22:00 +0900

    add 페이지 호출 시 리스트 state 초기화

--- client/src/components/pages/main/collection/CollectionAddPage.tsx
+++ client/src/components/pages/main/collection/CollectionAddPage.tsx
@@ -1,6 +1,8 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { useSetRecoilState } from "recoil";
+import { collectionCartList } from "../../../../state/movieState";
 import MovieSearchPage from "../search/MovieSearchPage";
 
 // ----------------------------------------------------------------------
@@ -8,6 +10,10 @@ import MovieSearchPage from "../search/MovieSearchPage";
 // ----------------------------------------------------------------------
 
 const CollectionAddPage = () => {
+  const setCollectionCartList = useSetRecoilState(collectionCartList);
+
+  setCollectionCartList([]);
+
   return (
     <>
       <Helmet>
