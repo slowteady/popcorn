e92e8922d3c7ea3715ff5f28b19a0e709e77608a
slowteady <vbn0213@gmail.com>
2023-07-21 16:21:22 +0900

    테이블 헤더 타입 추가 및 로직 변경

--- client/src/components/layouts/tables/ListTableHead.tsx
+++ client/src/components/layouts/tables/ListTableHead.tsx
@@ -1,30 +1,31 @@
 import { Checkbox, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
+import {
+  ListTableHeadLabelProps,
+  ListTableHeadProps,
+} from "../../../types/movies/movieTypes";
 
 // ----------------------------------------------------------------------
 // 테이블 헤더
 // ----------------------------------------------------------------------
 
-const visuallyHidden = {
-  border: 0,
-  margin: -1,
-  padding: 0,
-  width: "1px",
-  height: "1px",
-  overflow: "hidden",
-  position: "absolute",
-  whiteSpace: "nowrap",
-  clip: "rect(0 0 0 0)",
-};
-
-const ListTableHead = ({ headLabel }: any) => {
+const ListTableHead = ({
+  headLabel,
+  rowCount,
+  numSelected,
+  onSelectAllClick,
+}: ListTableHeadProps) => {
   return (
     <TableHead>
       <TableRow>
         <TableCell padding="checkbox">
-          <Checkbox />
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+          />
         </TableCell>
-        {headLabel.map((headCell: any) => (
+        {headLabel.map((headCell: ListTableHeadLabelProps) => (
           <TableCell
             key={headCell.id}
             align={headCell.alignRight ? "right" : "center"}
