35c9698daf4cdc18af42c2bbe405cf72b3ae5109
slowteady <vbn0213@gmail.com>
2023-07-23 15:27:01 +0900

    미들웨어 삽입

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { registerCollection } = require("../controllers/collectionController");
+const { auth } = require("../middleware/auth");
 const router = express.Router();
 
 // 등록
-router.post("/register", registerCollection);
+router.post("/register", auth, registerCollection);
 
 module.exports = router;
