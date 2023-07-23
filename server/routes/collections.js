c7f606852f4d4be045aa9038a13797e387ea6a2f
LeeYongMin01 <vbn0213@gmail.com>
2023-07-23 22:53:58 +0900

    컬렉션 목록 라우터 추가

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerCollection } = require("../controllers/collectionController");
+const {
+  registerCollection,
+  getCollection,
+} = require("../controllers/collectionController");
 const { auth } = require("../middleware/auth");
 const router = express.Router();
 
 // 등록
 router.post("/register", auth, registerCollection);
+
+// 목록
+router.get("/:page", auth, getCollection);
 
 module.exports = router;
