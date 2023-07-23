43daceb9d0c69aa0716fad9baed621c23c271c44
slowteady <vbn0213@gmail.com>
2023-07-23 16:19:30 +0900

    컬렉션 리스트 게시판 컴포넌트 추가

---
+++ client/src/components/pages/main/collection/CollectionListBoard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// ----------------------------------------------------------------------
+// 컬렉션 리스트 게시판형
+// ----------------------------------------------------------------------
+
+const CollectionListBoard = () => {
+  return <div>CollectionListBoard</div>;
+};
+
+export default CollectionListBoard;
