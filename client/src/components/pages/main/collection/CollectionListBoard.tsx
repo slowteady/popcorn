a6efc5215bb2bbabcdc0967dac725c30c1be9251
slowteady <vbn0213@gmail.com>
2023-07-23 16:58:34 +0900

    컬렉션 리스트 테이블 헤더 추가

--- client/src/components/pages/main/collection/CollectionListBoard.tsx
+++ client/src/components/pages/main/collection/CollectionListBoard.tsx
@@ -1,11 +1,36 @@
+import { Box, Card, Table, TableContainer } from "@mui/material";
 import React from "react";
+import ListTableHead from "../../../layouts/tables/ListTableHead";
 
 // ----------------------------------------------------------------------
 // 컬렉션 리스트 게시판형
 // ----------------------------------------------------------------------
 
+// 한 페이지 행 갯수
+const ROWSPERPAGE = 10;
+
+// 테이블 헤더 Config
+const TABLE_HEAD = [
+  { id: "collection", label: "컬렉션", alignRight: false },
+  { id: "rgstDate", label: "등록일자", alignRight: false },
+  { id: "author", label: "큐레이터", alignRight: false },
+];
+
 const CollectionListBoard = () => {
-  return <div>CollectionListBoard</div>;
+  return (
+    <Card sx={{ maxHeight: "550px", mt: "70px" }}>
+      <Box>
+        <TableContainer>
+          <Table size="medium">
+            <ListTableHead
+              headLabel={TABLE_HEAD}
+              sx={{ backgroundColor: "#e1f0ff" }}
+            />
+          </Table>
+        </TableContainer>
+      </Box>
+    </Card>
+  );
 };
 
 export default CollectionListBoard;
