cca01d2039f1faa9ad2097fa10c028c5acd308f0
slowteady <vbn0213@gmail.com>
2023-07-27 09:54:32 +0900

    삭제 기능 서비스 로직 구현

--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -251,3 +251,27 @@ export const getDetailData = async (
     };
   }
 };
+
+// 컬렉션 삭제 요청
+export const deleteCollection = async (id: string) => {
+  try {
+    const url = "/api/collections";
+    const response = await axios.delete(url, {
+      params: {
+        id,
+      },
+    });
+
+    const obj = { payload: response.data };
+    if (response.data.msg && response.data.msg.code) {
+      // 에러 코드 있을 시
+      obj.payload.code = response.data.msg.code;
+    }
+    return obj;
+  } catch (err) {
+    console.error(err);
+    return {
+      payload: { isSuccess: false, msg: "오류가 발생했어요" },
+    };
+  }
+};
