1276bcca509515b60311d6c583e4c52b3e4c7a39
slowteady <vbn0213@gmail.com>
2023-07-30 20:36:02 +0900

    리펙토링

--- client/src/components/pages/users/ProfileForm.tsx
+++ client/src/components/pages/users/ProfileForm.tsx
@@ -1,19 +1,19 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import React, { FormEvent, memo, useEffect, useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { updateProfileUser } from "../../../services/userService";
 import { userData } from "../../../state/userState";
 import { ProfileFormProps } from "../../../types/state/users/profileTypes";
+import { isSuccessValidate } from "../../../utils/auth/responseValidate";
 
 // ----------------------------------------------------------------------
 // 프로파일 Form 컴포넌트
 // ----------------------------------------------------------------------
 
 const ProfileForm = ({ avatarImg }: ProfileFormProps) => {
-  const usrData = useRecoilValue(userData);
-  const setData = useSetRecoilState(userData);
-  const { intro, name, email } = usrData;
+  const [usrData, setUsrData] = useRecoilState(userData);
   const [selfIntro, setSelfIntro] = useState("");
+  const { intro, name, email } = usrData;
 
   useEffect(() => {
     setSelfIntro(intro);
@@ -22,20 +22,22 @@ const ProfileForm = ({ avatarImg }: ProfileFormProps) => {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const updateData = { selfIntro, avatarImg };
-    const updateResult = await updateProfileUser(updateData);
-    const payload = updateResult.payload;
-    const { isSuccess, user } = payload;
+    const body = { selfIntro, avatarImg };
+    const response = await updateProfileUser(body);
+    const { isSuccess, user } = response.payload;
+    const isComplete = isSuccessValidate(response);
 
-    if (isSuccess) {
+    if (isComplete && isSuccess) {
       // 전역 유저 객체 업데이트
-      setData(user);
+      setUsrData(user);
     }
   };
 
   return (
     <>
       <Box
+        component="form"
+        onSubmit={handleSubmit}
         sx={{
           mt: 3,
           display: "flex",
@@ -43,8 +45,6 @@ const ProfileForm = ({ avatarImg }: ProfileFormProps) => {
           alignItems: "center",
           width: "80%",
         }}
-        component="form"
-        onSubmit={handleSubmit}
       >
         <Typography color="black" fontSize={18}>
           {name}
@@ -53,19 +53,19 @@ const ProfileForm = ({ avatarImg }: ProfileFormProps) => {
           {email}
         </Typography>
         <TextField
-          label="자기소개"
-          value={selfIntro}
           onChange={(e) => setSelfIntro(e.target.value)}
           inputProps={{ maxLength: 100 }}
+          value={selfIntro}
+          label="자기소개"
+          rows={8}
           multiline
-          rows={5}
           sx={{ mt: 2, width: "100%" }}
         />
         <Button
           type="submit"
           variant="contained"
-          size="large"
-          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          sx={{ mt: 4, mb: 2 }}
         >
           저장
         </Button>
