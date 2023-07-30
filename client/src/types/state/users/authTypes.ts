0ec8ace9e1234421877a99c79d9a1b984c987ebf
slowteady <vbn0213@gmail.com>
2023-07-30 16:41:15 +0900

    Payload 관련 타입 전역 타입과 중복으로 제거

--- client/src/types/state/users/authTypes.ts
+++ client/src/types/state/users/authTypes.ts
@@ -4,15 +4,6 @@ import { ReactNode } from "react";
 // 사용자 검증 타입
 // ----------------------------------------------------------------------
 
-export interface AuthPayload {
-  payload: {
-    isSuccess: boolean;
-    msg: string | unknown;
-    user?: object | boolean;
-    isExpire?: boolean;
-  };
-}
-
 export interface AuthProps {
   children: ReactNode;
 }
