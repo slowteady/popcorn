f90e0a9c976256af6d9168256890b135dac4bf1c
slowteady <vbn0213@gmail.com>
2023-07-30 15:56:48 +0900

    불필요한 타입 제거

--- client/src/types/state/users/loginTypes.ts
+++ client/src/types/state/users/loginTypes.ts
@@ -2,11 +2,6 @@
 // 로그인 타입
 // ----------------------------------------------------------------------
 
-export interface LoginFormObj {
-  Email: string;
-  Password: string;
-}
-
 export interface LoginBody {
   email: string;
   password: string;
