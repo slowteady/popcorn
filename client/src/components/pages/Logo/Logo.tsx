c2fd92bad4dfe53e9895d560e8d8dc9b31bb1599
slowteady <vbn0213@gmail.com>
2023-07-30 20:42:16 +0900

    불필요한 소스 제거

--- client/src/components/pages/Logo/Logo.tsx
+++ client/src/components/pages/Logo/Logo.tsx
@@ -1,6 +1,5 @@
 import { Box, Link } from "@mui/material";
-import { SxProps } from "@mui/system";
-import React, { FunctionComponent } from "react";
+import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import logoImg from "../../../img/popcorn_icon.png";
 
@@ -8,27 +7,18 @@ import logoImg from "../../../img/popcorn_icon.png";
 // 로고 컴포넌트
 // ----------------------------------------------------------------------
 
-interface LogoProps {
-  sx?: SxProps;
-}
-
-const Logo: FunctionComponent<LogoProps> = ({ sx }) => {
-  const logo = (
-    <Box
-      component="img"
-      src={logoImg}
-      sx={{
-        width: 50,
-        height: 50,
-        cursor: "pointer",
-        ...sx,
-      }}
-    />
-  );
-
+const Logo = () => {
   return (
     <Link to="/main" component={RouterLink} sx={{ display: "contents" }}>
-      {logo}
+      <Box
+        component="img"
+        src={logoImg}
+        sx={{
+          width: 50,
+          height: 50,
+          cursor: "pointer",
+        }}
+      />
     </Link>
   );
 };
