97b66fc9a0f65d9e965d66b75ca9e99d20ffdfff
slowteady <vbn0213@gmail.com>
2023-08-01 11:41:43 +0900

    컬렉션 타입 분기 처리

---
+++ client/src/types/state/collection/collectionTypes.ts
@@ -0,0 +1,8 @@
+// ----------------------------------------------------------------------
+// collection 타입
+// ----------------------------------------------------------------------
+
+export interface CollectionAlbumListProps {
+  isCollection: boolean;
+  query: string;
+}
