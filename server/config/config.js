4ba41c39dea1ac50579eb891d41f039b8a5a3282
slowteady <vbn0213@gmail.com>
2023-08-01 13:56:06 +0900

    운영체제 관련 분기처리된 로직 제거

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,11 @@
 /* 
 .env 파일 설정 값
-1. NODE_ENV=환경 => package.json/scripts에 설정
+1. NODE_ENV=환경 => package.json/scripts에 설정, ex) NODE_ENV=development ./server/index.js
 2. SERVER_PORT=포트번호
 3. MONGO_URI=MongoDB 커넥트 연결 정보
 4. UPLOAD_PATH=저장소 PATH
 */
 const dotenv = require("dotenv");
-const os = require("os");
 const path = require("path");
 const rootPath = path.resolve(__dirname, "../.."); // 절대경로
 
@@ -23,11 +22,8 @@ const init = () => {
 
 init();
 
-// 운영체제에 따른 repository path 설정, 배포 후 window 분기 처리 제거 예정
+// 운영체제에 따른 repository path 설정
 let repo = process.env.UPLOAD_PATH;
-if (os.platform() === "win32") {
-  repo = "c:/popcorn-repository";
-}
 
 module.exports = {
   init,
