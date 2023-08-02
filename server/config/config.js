a82d2896c9ffb4d8b34c866b9eac10d061a55d96
slowteady <vbn0213@gmail.com>
2023-08-02 14:55:11 +0900

    로그 생성 로직 추가

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,13 @@
 2. SERVER_PORT=포트번호
 3. MONGO_URI=MongoDB 커넥트 연결 정보
 4. UPLOAD_PATH=저장소 PATH
+5. LOG_PATH=로그 저장 PATH
 */
 const dotenv = require("dotenv");
 const path = require("path");
 const rootPath = path.resolve(__dirname, "../.."); // 절대경로
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
 
 let envFile = ".env.development";
 if (process.env.NODE_ENV === "production") {
@@ -22,10 +25,26 @@ const init = () => {
 
 init();
 
+// 로그 생성
+if (process.env.NODE_ENV === "production") {
+  const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+      new DailyRotateFile({
+        filename: `${process.env.LOG_PATH}/server-%DATE%.log`,
+        datePattern: "YYYY-MM-DD",
+      }),
+    ],
+  });
+  logger.info("========LOG START========");
+}
+
 // 운영체제에 따른 repository path 설정
 let repo = process.env.UPLOAD_PATH;
 
 module.exports = {
   init,
   repo,
+  logger,
 };
--- server/index.js
+++ server/index.js
@@ -21,6 +21,13 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+// 포트번호 설정
+const port = process.env.SERVER_PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`========Server start on ${port}========`);
+});
+
 // 정적 파일 제공을 위한 디렉토리 설정
 app.use("/image", express.static(repo));
 
@@ -32,10 +39,3 @@ app.use("/api/users", require("./routes/users"));
 
 // 컬렉션 라우터
 app.use("/api/collections", require("./routes/collections"));
-
-// 포트번호 설정
-const port = process.env.SERVER_PORT || 8080;
-
-app.listen(port, () => {
-  console.log(`========Server start on ${port}========`);
-});
