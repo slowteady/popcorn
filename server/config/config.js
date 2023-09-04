33ec336f2c60954dbc74429246f5fcc2b0df6644
slowteady <vbn0213@gmail.com>
2023-09-04 21:14:42 +0900

    feat: 운영체제에 따른 기본 레포지토리 설정

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,5 @@
-/* 
-.env 파일 설정 값
-1. NODE_ENV=환경 => package.json/scripts에 설정, ex) NODE_ENV=development ./server/index.js
-2. SERVER_PORT=포트번호
-3. MONGO_URI=MongoDB 커넥트 연결 정보
-4. UPLOAD_PATH=저장소 PATH
-5. LOG_PATH=로그 저장 PATH
-*/
 const dotenv = require("dotenv");
+const os = require("os");
 const path = require("path");
 const rootPath = path.resolve(__dirname, "../.."); // 절대경로
 const winston = require("winston");
@@ -25,7 +18,6 @@ const init = () => {
 
 init();
 
-// 로그 생성
 if (process.env.NODE_ENV === "production") {
   const logger = winston.createLogger({
     level: "info",
@@ -40,8 +32,8 @@ if (process.env.NODE_ENV === "production") {
   logger.info("========LOG START========");
 }
 
-// 운영체제에 따른 repository path 설정
-let repo = process.env.UPLOAD_PATH;
+const repo =
+  os.platform() === "win32" ? "c:/repository" : process.env.UPLOAD_PATH;
 
 module.exports = {
   init,
