389a216fd7ebe55708fa52fc1b82ee4e91b44d74
slowteady <vbn0213@gmail.com>
2023-09-24 13:54:25 +0900

    feat: react-query provider 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import ThemeProvider from './components/theme/Index';
@@ -8,14 +9,25 @@ const App = () => {
   return (
     <HelmetProvider>
       <ThemeProvider>
-        <BrowserRouter>
-          <RecoilRoot>
-            <Router />
-          </RecoilRoot>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <RecoilRoot>
+              <Router />
+            </RecoilRoot>
+          </BrowserRouter>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
 };
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1
+    }
+  }
+});
 
 export default App;
