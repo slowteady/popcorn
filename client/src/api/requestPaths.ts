b74b132862a934ac5b3cde0ae5bdf99cfd8d2f6a
slowteady <vbn0213@gmail.com>
2023-09-25 10:21:48 +0900

    feat: axios 인스턴스 생성, api path 파일로 분기

---
+++ client/src/api/requestPaths.ts
@@ -0,0 +1,11 @@
+export const MOVIE_PATH = {
+  IMAGE_SIZE_300: '/w300',
+  IMAGE_SIZE_500: '/w500',
+  IMAGE_SIZE_780: '/w780',
+  IMAGE_SIZE_ORIGINAL: '/original',
+  POPULAR_PATH: '/movie/popular',
+  NOWPLAYING_PATH: '/movie/now_playing',
+  TOP_RATED: '/movie/top_rated',
+  SEARCH_PATH: '/search/movie',
+  CREDITS_PATH: '/credits'
+};
