b0d6f5b16c98785802f16c2bb31d08bab5b6186a
slowteady <vbn0213@gmail.com>
2023-09-27 23:35:46 +0900

    chore: 상수 재사용을 위해 export 처리

--- client/src/api/requestInstance.ts
+++ client/src/api/requestInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-const IMAGE_URL = process.env.REACT_APP_IMAGE_BASE_URL;
+export const IMAGE_URL = process.env.REACT_APP_IMAGE_BASE_URL;
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 
 const defaultOptions = {
