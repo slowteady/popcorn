f82813ed27da04928903c197a499d71f1aa023ae
slowteady <vbn0213@gmail.com>
2023-09-27 23:37:01 +0900

    feat: 메인 페이지 포스터 컴포넌트 추가

--- client/src/components/sign/Poster.tsx
+++ client/src/components/sign/Poster.tsx
@@ -1,15 +1,55 @@
+import { Box, styled } from '@mui/material';
 import { useQuery } from 'react-query';
-import { getPoster } from '../../service/signService';
-import Loading from '../theme/Loading';
+import { IMAGE_URL } from '../../api/requestInstance';
+import { MOVIE_PATH } from '../../api/requestPaths';
+import { getMovie } from '../../service/movieService';
+import { Movie } from '../../types/movie';
+import { DataErrorIcon } from '../common/DataError';
+import Loading from '../common/Loading';
 
 const Poster = () => {
-  const { data, status } = useQuery(['getPopular'], () => getPoster());
+  const { data, status } = useQuery(['getRandomPoster', POPULAR_PATH], async () => {
+    const movieData = await getMovie(POPULAR_PATH);
+    if (!movieData) {
+      throw new Error();
+    }
+    const results = movieData.data.results;
+    const posterPaths = getPosterPaths(results);
+    const randomPosterPath = getRandomPosterPath(posterPaths);
+    const posterUrl = `${IMAGE_URL}${IMAGE_SIZE_ORIGINAL}${randomPosterPath}`;
+
+    return posterUrl;
+  });
 
   if (status === 'loading') {
     return <Loading />;
+  } else if (status === 'error') {
+    return (
+      <StyleDiv>
+        <DataErrorIcon icon='ph:file-x-bold' size={128} message={ERORR_MESSAGE} />
+      </StyleDiv>
+    );
   }
-  
-  return <div>Poster</div>;
 };
+
+const getPosterPaths = (results: Array<Movie>) => {
+  return results.map((movie: Movie) => movie.poster_path);
+};
+
+const getRandomPosterPath = (posterPaths: string[]) => {
+  return posterPaths[Math.floor(Math.random() * posterPaths.length)];
+};
+
+const { POPULAR_PATH, IMAGE_SIZE_ORIGINAL } = MOVIE_PATH;
+const ERORR_MESSAGE = '데이터 호출에 실패하였습니다.';
+
+const StyleDiv = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%'
+});
 
 export default Poster;
