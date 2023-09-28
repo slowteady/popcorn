90708590d3251118bc668a7db8341e4e3b7643b0
slowteady <vbn0213@gmail.com>
2023-09-28 23:46:50 +0900

    css: Grid 크기 수정

--- client/src/pages/sign/SignInPage.tsx
+++ client/src/pages/sign/SignInPage.tsx
@@ -1,4 +1,4 @@
-import { Grid, Hidden } from '@mui/material';
+import { Grid, Hidden, Paper } from '@mui/material';
 import Helmet from '../../components/common/Helmet';
 import Poster from '../../components/sign/Poster';
 import SignIn from '../../components/sign/SignIn';
@@ -9,18 +9,22 @@ const SignInPage = () => {
   return (
     <>
       <Helmet text={PAGE_TITLE} />
-      <Grid container component='main'>
+      <Grid container component='main' sx={fullHeightSx}>
         <Hidden mdDown>
           <Grid item md={5} lg={6}>
             <Poster />
           </Grid>
         </Hidden>
-        <Grid item md={7} lg={6}>
+        <Grid item square component={Paper} xs={12} sm={12} md={7} lg={6} elevation={6}>
           <SignIn />
         </Grid>
       </Grid>
     </>
   );
+};
+
+const fullHeightSx = {
+  height: '100vh'
 };
 
 export default SignInPage;
