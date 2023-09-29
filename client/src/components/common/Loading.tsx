9a606525ae6aa2db97bc175eb1c065de590aceb9
slowteady <vbn0213@gmail.com>
2023-09-30 00:06:29 +0900

    css: Poster 컴포넌트의 중앙 위치하도록 css 변경

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -12,10 +12,6 @@ const CenterDiv = styled(Box)({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
   backgroundColor: 'rgba(255, 255, 255, 0.9)',
   height: '100%',
   width: '100%'
