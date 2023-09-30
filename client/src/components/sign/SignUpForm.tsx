365fc226556d49ce7589a2142aebac42bdf12826
slowteady <vbn0213@gmail.com>
2023-09-30 13:44:04 +0900

    feat: 회원가입 form 렌더링 및 버튼 disabled 기능 추가

--- client/src/components/sign/SignUpForm.tsx
+++ client/src/components/sign/SignUpForm.tsx
@@ -1,6 +1,7 @@
-import { Button, FormControl, FormGroup, TextField } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { Button, FormControl, FormGroup, FormHelperText, TextField } from '@mui/material';
+import { ChangeEvent, FormEvent, Fragment, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signValidation, strValidation } from '../../utils/validation';
 
 const initialValue = {
   email: '',
@@ -18,11 +19,24 @@ const SignUpform = () => {
     { type: 'password', name: 'password', label: '패스워드', value: password },
     { type: 'password', name: 'confirmPassword', label: '패스워드 확인', value: confirmPassword }
   ];
+  const [isAble, setIsAble] = useState(false);
   const navigate = useNavigate();
 
   const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
+
+    const isAble = inputFields.every((field) => {
+      const { value, type } = field;
+      const validation = strValidation.isNotEmpty(value) && signValidation.signValidate(type, value);
+      return validation && validation.isValid;
+    });
+
+    setIsAble(isAble);
+  };
+
+  const doSignUp = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
   };
 
   const naviToSignIn = () => {
@@ -30,26 +44,36 @@ const SignUpform = () => {
   };
 
   return (
-    <FormControl fullWidth component='form'>
+    <FormControl fullWidth onSubmit={doSignUp} component='form'>
       <FormGroup>
         {inputFields.map((field, index) => {
           const { type, name, label, value } = field;
+          const validation = strValidation.isNotEmpty(value) && signValidation.signValidate(type, value);
+
           return (
-            <TextField
-              key={index}
-              required
-              margin='normal'
-              autoComplete='new-password'
-              onChange={inputChange}
-              type={type}
-              name={name}
-              label={label}
-              value={value}
-            />
+            <Fragment key={index}>
+              <TextField
+                key={`${index}-field`}
+                required
+                margin='normal'
+                autoComplete='new-password'
+                onChange={inputChange}
+                type={type}
+                name={name}
+                label={label}
+                value={value}
+                error={validation ? !validation.isValid : false}
+              />
+              {validation && !validation.isValid && (
+                <FormHelperText key={`${index}-helper`} sx={{ color: '#e91b1b', fontWeight: 'bold' }}>
+                  {validation.errorMessage}
+                </FormHelperText>
+              )}
+            </Fragment>
           );
         })}
       </FormGroup>
-      <Button type='submit' variant='contained' size='large' fullWidth sx={{ mt: 2 }}>
+      <Button type='submit' variant='contained' size='large' disabled={!isAble} fullWidth sx={{ mt: 2 }}>
         회원가입
       </Button>
       <Button type='button' variant='contained' size='large' onClick={naviToSignIn} sx={buttonSx}>
