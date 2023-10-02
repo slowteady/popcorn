fdd705709b98f3fa01663357e16969228913c109
slowteady <vbn0213@gmail.com>
2023-10-02 12:37:37 +0900

    feat: sign 관련 api 인스턴스 생성 로직 파일로 분기

---
+++ client/src/api/sign/signApiInstance.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = '/api/users';
+
+const defaultOptions = {
+  timeout: 1000,
+  baseURL: BASE_URL
+};
+
+export const signRequest = axios.create({
+  ...defaultOptions
+});
