9e17e5a03742c9b6f3513c1576ea9b61c17dc40a
slowteady <vbn0213@gmail.com>
2023-10-02 12:49:17 +0900

    refactor: 타입 분리

--- client/src/types/sign.d.ts
+++ client/src/types/sign.d.ts
@@ -1,6 +1,8 @@
-export interface SignUpForm {
+export interface BasicForm {
   email: string;
   password: string;
+}
+export interface SignUpForm extends BasicForm {
   name: string;
   confirmPassword: string;
 }
