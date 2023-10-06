33a1bd5ad9fc1590fbe4228570aba82a53580f6a
slowteady <vbn0213@gmail.com>
2023-10-06 22:09:21 +0900

    feat: 메인 레이아웃 컴포넌트 추가

---
+++ client/src/components/main/layout/MainLayout.tsx
@@ -0,0 +1,33 @@
+import { styled } from '@mui/material';
+import { useState } from 'react';
+import { ReactNodeProps } from '../../../types/global';
+
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 92;
+
+const MainLayout = ({ children }: ReactNodeProps) => {
+  const [open, setOpen] = useState(false);
+
+  return <Main>{children}</Main>;
+};
+
+const StyledRoot = styled('div')({
+  display: 'flex',
+  minHeight: '100%',
+  overflow: 'hidden'
+});
+
+const Main = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  overflow: 'auto',
+  minHeight: '100%',
+  paddingTop: APP_BAR_MOBILE + 24,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: APP_BAR_DESKTOP + 24,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
+  }
+}));
+
+export default MainLayout;
