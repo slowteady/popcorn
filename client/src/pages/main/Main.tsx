98dc6a7c8b7fc2077acdfca2afca9999c3656ee8
slowteady <vbn0213@gmail.com>
2023-10-06 22:42:52 +0900

    feat: 메인 레이아웃 렌더링

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import MainLayout from '../../components/layouts/MainLayout';
 
 const Main = () => {
-  return <Outlet />;
+  return (
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
+  );
 };
 
 export default Main;
