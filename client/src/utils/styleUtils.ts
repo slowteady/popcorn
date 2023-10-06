55d90751aa0ec66e19b9abbeed8db53ba8b6ca13
slowteady <vbn0213@gmail.com>
2023-10-06 22:40:34 +0900

    feat: 스타일 유틸을 위한 함수 추가

---
+++ client/src/utils/styleUtils.ts
@@ -0,0 +1,37 @@
+import { alpha } from '@mui/material';
+
+interface BgBlurProps {
+  color: string;
+  blur: number;
+  opacity: number;
+}
+
+export const bgBlur = (props: BgBlurProps) => {
+  const color = props.color || '#000000';
+  const blur = props.blur || 6;
+  const opacity = props.opacity || 0.8;
+
+  return {
+    backdropFilter: `blur(${blur}px)`,
+    WebkitBackdropFilter: `blur(${blur}px)`,
+    backgroundColor: alpha(color, opacity)
+  };
+};
+
+export const whichContainerSize = (isCollection: boolean) => {
+  return isCollection
+    ? {
+        itemSize: { xs: 8, sm: 7, md: 6, lg: 6 },
+        itemSx: { p: 1 },
+        cardSx: { p: 1, cursor: 'pointer' },
+        cartSize: { xs: 4, sm: 5, md: 6, lg: 6 },
+        gridSx: { maxHeight: '525px', overflowY: 'auto', mt: 5 },
+        boxSx: { minHeight: '200px', position: 'relative' }
+      }
+    : {
+        containerSize: { spacing: 2 },
+        itemSize: { xs: 8, sm: 6, md: 3, lg: 3 },
+        cardSx: { cursor: 'pointer' },
+        boxSx: { minHeight: '405px', position: 'relative' }
+      };
+};
