f30c96facb2cd304747c84ac3f32ee8e8acba2b6
slowteady <vbn0213@gmail.com>
2023-10-07 13:55:21 +0900

    feat: 반응형을 위한 커스텀 훅 추가

---
+++ client/src/hooks/useResponsive.tsx
@@ -0,0 +1,33 @@
+import { Breakpoint, useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+interface useResponsiveProps {
+  query: 'up' | 'down' | 'between';
+  start: Breakpoint;
+  end?: number | Breakpoint;
+}
+
+export const useResponsive = ({ query, start, end }: useResponsiveProps) => {
+  const theme = useTheme();
+
+  const mediaUp = useMediaQuery(theme.breakpoints.up(start));
+  const mediaDown = useMediaQuery(theme.breakpoints.down(start));
+  let mediaBetween = false;
+
+  const mediaOnly = useMediaQuery(theme.breakpoints.only(start));
+
+  if (query === 'up') {
+    return mediaUp;
+  }
+
+  if (query === 'down') {
+    return mediaDown;
+  }
+
+  if (query === 'between' && end) {
+    mediaBetween = useMediaQuery(theme.breakpoints.between(start, end));
+    return mediaBetween;
+  }
+
+  return mediaOnly;
+};
