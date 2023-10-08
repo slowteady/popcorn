b1161ac237deb393b9fd43c8e990ac84df50f7ac
slowteady <vbn0213@gmail.com>
2023-10-08 23:51:27 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ client/src/components/icon/Icon.tsx
@@ -0,0 +1,15 @@
+import { IconifyIcon } from '@iconify/react';
+import { Box, SxProps } from '@mui/material';
+
+interface IconProps {
+  icon: string | IconifyIcon;
+  width?: number;
+  sx?: SxProps;
+  onClick?: React.MouseEventHandler;
+}
+
+const Icon = ({ icon, width = 20, sx, ...other }: IconProps) => {
+  return <Box component={Icon} icon={icon} sx={{ width, height: width, ...sx }} {...other} />;
+};
+
+export default Icon;
