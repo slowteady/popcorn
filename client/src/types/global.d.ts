4c2cf94dd0b567f6443c43f571a495faa87661a0
slowteady <vbn0213@gmail.com>
2023-10-08 22:39:08 +0900

    feat: 타입스크립트가 이미지 인식하도록 모듈 구문 추가

--- client/src/types/global.d.ts
+++ client/src/types/global.d.ts
@@ -1,3 +1,10 @@
+export declare global {
+  declare module '*.png';
+  declare module '*.jpg';
+  declare module '*.jpeg';
+  declare module '*.svg';
+}
+
 export interface ReactNodeProps {
   children: ReactNode;
 }
