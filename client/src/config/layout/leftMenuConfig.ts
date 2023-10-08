25fd14423879081e86b3cae3d4e4913265212e15
slowteady <vbn0213@gmail.com>
2023-10-08 23:52:50 +0900

    feat: 좌측메뉴 설정 파일 추가

---
+++ client/src/config/layout/leftMenuConfig.ts
@@ -0,0 +1,19 @@
+const leftmenuConfig = [
+  {
+    title: 'Movies',
+    path: '/main/movies',
+    icon: 'material-symbols:movie'
+  },
+  {
+    title: 'Search',
+    path: '/main/search',
+    icon: 'eva:search-fill'
+  },
+  {
+    title: 'Collection',
+    path: '/main/collection',
+    icon: 'wpf:books'
+  }
+];
+
+export default leftmenuConfig;
