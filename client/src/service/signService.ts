0b7bbd4643ab49236afac217f39ecc52d3c6dab2
slowteady <vbn0213@gmail.com>
2023-10-13 23:51:38 +0900

    feat: 리턴 값 타입 지정

--- client/src/service/signService.ts
+++ client/src/service/signService.ts
@@ -1,12 +1,25 @@
+import { AxiosResponse } from 'axios';
 import { signRequest } from '../api/sign/signApiInstance';
 import { BasicForm, SignUpForm } from '../types/sign';
+
+export interface userDataType {
+  user: {
+    id: string;
+    email: string;
+    image: string;
+    intro: string;
+    name: string;
+  };
+}
 
 export const registerUser = (body: SignUpForm) => {
   return signRequest.post('/register', body);
 };
+
 export const loginUser = (body: BasicForm) => {
   return signRequest.post('/login', body);
 };
-export const authCheck = () => {
+
+export const authCheck = (): Promise<AxiosResponse<userDataType>> => {
   return signRequest.get('/auth');
 };
