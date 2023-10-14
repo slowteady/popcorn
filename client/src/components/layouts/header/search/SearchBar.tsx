c83868401d0cad7adfe23d4e965a2ee49ddb4db0
slowteady <vbn0213@gmail.com>
2023-10-14 21:07:52 +0900

    feat: 상단 헤더 컴포넌트 추가, 레이아웃 구현 완료

--- client/src/components/layouts/header/search/SearchBar.tsx
+++ client/src/components/layouts/header/search/SearchBar.tsx
@@ -1,35 +1,34 @@
-import { ClickAwayListener, IconButton } from '@mui/material';
+import { ClickAwayListener, IconButton, Slide } from '@mui/material';
 import { useRef, useState } from 'react';
 import Icon from '../../../common/icon/Icon';
 import SearchBarSlide from './SearchBarSlide';
 
-// 1. 레이아웃 잡고 ref를 자식 컴포넌트에 전달
-// 2. 클릭했을 때 나올 레이아웃이 자식 컴포넌트
-
-const ICON_NAME = 'eva:search-fill';
+export const ICON_NAME = 'eva:search-fill';
 
 const SearchBar = () => {
   const [open, setOpen] = useState(false);
-  const ref = useRef(null);
+  const ref = useRef<HTMLInputElement>(null);
 
   const openSearchSlide = () => {
     setOpen(true);
   };
 
   const closeSearchSlide = () => {
-    setOpen(false);
+    ref.current && setOpen(false);
   };
 
   return (
-    <ClickAwayListener onClickAway={closeSearchSlide}>
-      <>
+    <ClickAwayListener onClickAway={closeSearchSlide} disableReactTree>
+      <div>
         {!open && (
           <IconButton onClick={openSearchSlide}>
             <Icon icon={ICON_NAME} />
           </IconButton>
         )}
-        <SearchBarSlide open={open} setOpen={setOpen} ref={ref} />
-      </>
+        <Slide direction='down' in={open} mountOnEnter unmountOnExit>
+          <SearchBarSlide setOpen={setOpen} ref={ref} />
+        </Slide>
+      </div>
     </ClickAwayListener>
   );
 };
