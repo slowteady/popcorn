8076f7b3b1a0046763b290fb2f7a9647a619c2f7
slowteady <vbn0213@gmail.com>
2023-10-15 15:42:20 +0900

    chore: 컴포넌트명 변경

--- client/src/components/layouts/header/UserPopover.tsx
+++
@@ -1,5 +0,0 @@
-const UserPopover = () => {
-  return <div>UserPopover</div>;
-};
-
-export default UserPopover;
---
+++ client/src/components/layouts/header/UserProfileBtn.tsx
@@ -0,0 +1,5 @@
+const UserProfileBtn = () => {
+  return <div>UserPopover</div>;
+};
+
+export default UserProfileBtn;
