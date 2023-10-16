d797eb28286bc69ab3e84cb5c85df0322d5a5072
slowteady <vbn0213@gmail.com>
2023-10-16 17:05:53 +0900

    feat: 프로파일 버튼 컴포넌트 추가

--- client/src/components/layouts/header/UserProfileBtn.tsx
+++ client/src/components/layouts/header/UserProfileBtn.tsx
@@ -1,5 +1,51 @@
+import { Avatar, IconButton, alpha } from '@mui/material';
+import { MouseEvent, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { USER_MAIN_OPTION, userSelector } from '../../../state/userState';
+import UserProfileLayer from './UserProfileLayer';
+
 const UserProfileBtn = () => {
-  return <div>UserPopover</div>;
+  const { image } = useRecoilValue(userSelector(USER_MAIN_OPTION));
+  const [open, setOpen] = useState(false);
+  const [element, setElement] = useState<HTMLButtonElement | null>(null);
+
+  const onLayer = (e: MouseEvent<HTMLButtonElement>) => {
+    setOpen(true);
+    setElement(e.currentTarget);
+  };
+
+  const closeLayer = () => {
+    setOpen(false);
+    setElement(null);
+  };
+
+  return (
+    <>
+      <IconButton
+        onClick={onLayer}
+        sx={{
+          p: 0,
+          '&:hover': {
+            bgcolor: '#b6d4b6'
+          },
+          ...(open && {
+            '&:before': {
+              zIndex: 1,
+              content: "''",
+              width: '100%',
+              height: '100%',
+              borderRadius: '50%',
+              position: 'absolute',
+              bgcolor: (theme) => alpha(theme.palette.grey[900], 0.3)
+            }
+          })
+        }}
+      >
+        <Avatar src={image} />
+      </IconButton>
+      <UserProfileLayer element={element} closeLayer={closeLayer} />
+    </>
+  );
 };
 
 export default UserProfileBtn;
