1abfc0077af36803dc6e3a27197b6e3715bd5245
slowteady <vbn0213@gmail.com>
2023-10-18 21:22:20 +0900

    feat: 프로파일 이미지 추가 기능 구현

--- client/src/components/main/profile/ProfileForm.tsx
+++ client/src/components/main/profile/ProfileForm.tsx
@@ -1,20 +1,63 @@
 import { Avatar } from '@mui/material';
 import { green } from '@mui/material/colors';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { USER_PROFILE_OPTION, userSelector } from '../../../state/userState';
+import { errorHandler } from '../../../utils/exceptionHandler';
 import Icon from '../../common/icon/Icon';
 
 const IMG_CHANGE_ICON = 'material-symbols:change-circle-outline';
+const IMAGE_ERROR_MESSAGE = '지원하지 않는 이미지 형식입니다.';
 
 const ProfileForm = () => {
-  const { email, image, intro, name } = useRecoilValue(userSelector(USER_PROFILE_OPTION));
+  const { email, image: globalImage, intro: globalIntro, name } = useRecoilValue(userSelector(USER_PROFILE_OPTION));
+  const [image, setImage] = useState('');
+  const [introduce, setIntroduce] = useState('');
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    globalImage && setImage(globalImage);
+    globalIntro && setIntroduce(globalIntro);
+  }, [globalImage, globalIntro]);
+
+  const changeImage = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const insertImage = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files) {
+      const file = e.currentTarget.files[0];
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        try {
+          if (e.target) {
+            const result = e.target.result;
+            if (typeof result === 'string') {
+              setImage(result);
+            } else {
+              throw new Error(IMAGE_ERROR_MESSAGE);
+            }
+          }
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            errorHandler(error);
+          }
+        }
+      };
+    }
+  };
 
   return (
     <>
       <Avatar src={image} alt='profile' sx={userImgSx} />
-      <Avatar sx={changeIconSx}>
+      <Avatar onClick={changeImage} sx={changeIconSx}>
         <Icon icon={IMG_CHANGE_ICON} width={20} />
       </Avatar>
+      <input onChange={insertImage} ref={fileInputRef} type='file' hidden />
     </>
   );
 };
