77ffb0c6d97d24642800cdaabc134d9c2f3e9744
slowteady <vbn0213@gmail.com>
2023-10-18 14:55:43 +0900

    feat: 프로파일 페이지 컴포넌트 추가

--- client/src/pages/main/users/Profile.tsx
+++ client/src/pages/main/users/Profile.tsx
@@ -1,5 +1,18 @@
+import { Container } from '@mui/material';
+import Helmet from '../../../components/common/Helmet';
+import ProfileIndex from '../../../components/main/profile/ProfileIndex';
+
+const PAGE_TITLE = 'Profile';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <Helmet text={PAGE_TITLE} />
+      <Container component='main' maxWidth='sm'>
+        <ProfileIndex />
+      </Container>
+    </>
+  );
 };
 
 export default Profile;
