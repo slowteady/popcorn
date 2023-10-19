d61343e0978a05f1b8840e03423a4839c706838a
slowteady <vbn0213@gmail.com>
2023-10-19 17:50:44 +0900

    feat: atom 추가하여 구독한 컴포넌트들 리렌더링 돼도록 변경

--- client/src/state/userState.ts
+++ client/src/state/userState.ts
@@ -1,4 +1,4 @@
-import { selectorFamily } from 'recoil';
+import { atom, selector, selectorFamily } from 'recoil';
 import { authCheck } from '../service/userService';
 
 type FilterOption = 'Main' | 'Profile';
@@ -6,17 +6,32 @@ type FilterOption = 'Main' | 'Profile';
 export const USER_MAIN_OPTION = 'Main';
 export const USER_PROFILE_OPTION = 'Profile';
 
+const asyncSelector = selector({
+  key: 'asyncSelector',
+  get: async () => {
+    const response = await authCheck();
+    return response.data.user;
+  }
+});
+
+export const userAtom = atom({
+  key: 'userAtom',
+  default: asyncSelector
+});
+
 export const userSelector = selectorFamily({
   key: 'userSelector',
-  get: (option: FilterOption) => async () => {
-    const response = await authCheck();
-    const { email, image, intro, name } = response.data.user;
+  get:
+    (option: FilterOption) =>
+    async ({ get }) => {
+      const user = get(userAtom);
+      const { email, image, intro, name } = user;
 
-    switch (option) {
-      case USER_MAIN_OPTION:
-        return { image, name };
-      case USER_PROFILE_OPTION:
-        return { email, image, intro, name };
+      switch (option) {
+        case USER_MAIN_OPTION:
+          return { image, name };
+        case USER_PROFILE_OPTION:
+          return { email, image, intro, name };
+      }
     }
-  }
 });
