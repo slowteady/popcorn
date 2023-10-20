5cf7e922704429bb9ac62961686a0d632794bdc8
slowteady <vbn0213@gmail.com>
2023-10-20 22:59:02 +0900

    feat: 필터 조건 객체로 관리하도록 파일로 분기

---
+++ client/src/config/movie/movieConfig.ts
@@ -0,0 +1,7 @@
+import { FilterOption } from '../../types/movie';
+
+export const FILTER_OPTION: FilterOption[] = [
+  { value: 'NOWPLAYING', label: 'Now Playing' },
+  { value: 'POPULAR', label: 'Popular' },
+  { value: 'TOPRATED', label: 'Top Rated' }
+];
