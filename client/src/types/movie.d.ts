7d55d784752f7fe6735c45365a961b93d7ff8708
slowteady <vbn0213@gmail.com>
2023-10-20 22:57:02 +0900

    feat: 필터 타입 추가

--- client/src/types/movie.d.ts
+++ client/src/types/movie.d.ts
@@ -1,3 +1,10 @@
 export interface Movie {
   poster_path: string;
 }
+
+type Option = 'NOWPLAYING' | 'POPULAR' | 'TOPRATED';
+
+export interface FilterOption {
+  value: Option;
+  label: string;
+}
