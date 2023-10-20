395e44633fb345ca42ca2fa211708829357bc7b0
slowteady <vbn0213@gmail.com>
2023-10-20 22:57:24 +0900

    feat: movies 페이지 컴포넌트 추가

--- client/src/pages/main/movies/Movies.tsx
+++ client/src/pages/main/movies/Movies.tsx
@@ -1,5 +1,21 @@
+import { Container, Typography } from '@mui/material';
+import Helmet from '../../../components/common/Helmet';
+import MoviesIndex from '../../../components/main/movies/MoviesIndex';
+
+const PAGE_TITLE = 'Movies';
+
 const Movies = () => {
-  return <div>Movies</div>;
+  return (
+    <>
+      <Helmet text={PAGE_TITLE} />
+      <Container sx={{ mt: 1 }}>
+        <Typography variant='h4' sx={{ mb: 5 }}>
+          {PAGE_TITLE}
+        </Typography>
+        <MoviesIndex />
+      </Container>
+    </>
+  );
 };
 
 export default Movies;
