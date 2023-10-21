f4c927b4e5a06159d99831a5d210f1e8c3985076
slowteady <vbn0213@gmail.com>
2023-10-21 17:54:49 +0900

    feat: iconmsg 컴포넌트 확장

--- client/src/components/common/icon/IconMsg.tsx
+++ client/src/components/common/icon/IconMsg.tsx
@@ -1,17 +1,28 @@
 import { Icon } from '@iconify/react';
-import { Typography } from '@mui/material';
+import { Box, SxProps, Typography } from '@mui/material';
 
 interface IconMsgProps {
   icon: string;
   width?: number;
   message: string;
+  sx?: SxProps;
+  fontSx?: SxProps;
 }
 
-export const IconMsg = ({ icon, width = 48, message }: IconMsgProps) => {
+export const IconMsg = ({ icon, width = 48, message, sx = defaultSx, fontSx }: IconMsgProps) => {
   return (
-    <>
+    <Box sx={{ ...sx }}>
       <Icon icon={icon} width={width} height={width} />
-      <Typography m={2}>{message}</Typography>
-    </>
+      <Typography m={2} sx={{ ...fontSx }}>
+        {message}
+      </Typography>
+    </Box>
   );
+};
+
+const defaultSx = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
 };
