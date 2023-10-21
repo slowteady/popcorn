9f663c7795ef1ebc22769c4b2bd269b98aee3813
slowteady <vbn0213@gmail.com>
2023-10-21 15:42:31 +0900

    feat: 오른쪽 배치 적용을 위한 stack 로직 추가

--- client/src/components/main/movies/MoviesIndex.tsx
+++ client/src/components/main/movies/MoviesIndex.tsx
@@ -1,11 +1,26 @@
+import { Stack, styled } from '@mui/material';
+import MovieAlbum from './MovieAlbum';
 import MovieFilterLayer from './MovieFilterLayer';
 
 const MoviesIndex = () => {
   return (
     <>
-      <MovieFilterLayer />
+      <WrapStack>
+        <Stack direction='row' spacing={1} flexShrink={0} sx={{ my: 1 }}>
+          <MovieFilterLayer />
+        </Stack>
+      </WrapStack>
+      <MovieAlbum />
     </>
   );
 };
 
+const WrapStack = styled(Stack)(({ theme }) => ({
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap-reverse',
+  justifyContent: 'flex-end',
+  marginBottom: theme.spacing(3)
+}));
 export default MoviesIndex;
