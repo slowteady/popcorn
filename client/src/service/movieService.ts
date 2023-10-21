f25919db5cf69063192f70fe45375670a46555a2
slowteady <vbn0213@gmail.com>
2023-10-21 22:47:16 +0900

    feat: 페이지 인자 여부 분기처리

--- client/src/service/movieService.ts
+++ client/src/service/movieService.ts
@@ -1,5 +1,5 @@
 import { movieDataRequest } from '../api/movie/movieApiInstance';
 
-export const getMovie = (path: string) => {
-  return movieDataRequest.get(path);
+export const getMovie = (path: string, page?: number) => {
+  return page ? movieDataRequest.get(path, { params: page }) : movieDataRequest.get(path);
 };
