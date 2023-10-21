641dbddfd594dd8ef6e23337def9168ee999ea96
slowteady <vbn0213@gmail.com>
2023-10-21 22:04:42 +0900

    feat: movie 타입 추가

--- client/src/types/movie.d.ts
+++ client/src/types/movie.d.ts
@@ -8,3 +8,12 @@ export interface FilterOption {
   value: Option;
   label: string;
 }
+
+export interface MoviesData {
+  id: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+  genre_ids: number[];
+}
