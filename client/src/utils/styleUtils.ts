1e3e7282b291323f7043acff6609aaf69d34e7e0
slowteady <vbn0213@gmail.com>
2023-10-21 17:43:02 +0900

    chore: 사용하지 않는 함수 제거

--- client/src/utils/styleUtils.ts
+++ client/src/utils/styleUtils.ts
@@ -17,21 +17,3 @@ export const bgBlur = (props: BgBlurProps) => {
     backgroundColor: alpha(color, opacity)
   };
 };
-
-export const whichContainerSize = (isCollection: boolean) => {
-  return isCollection
-    ? {
-        itemSize: { xs: 8, sm: 7, md: 6, lg: 6 },
-        itemSx: { p: 1 },
-        cardSx: { p: 1, cursor: 'pointer' },
-        cartSize: { xs: 4, sm: 5, md: 6, lg: 6 },
-        gridSx: { maxHeight: '525px', overflowY: 'auto', mt: 5 },
-        boxSx: { minHeight: '200px', position: 'relative' }
-      }
-    : {
-        containerSize: { spacing: 2 },
-        itemSize: { xs: 8, sm: 6, md: 3, lg: 3 },
-        cardSx: { cursor: 'pointer' },
-        boxSx: { minHeight: '405px', position: 'relative' }
-      };
-};
