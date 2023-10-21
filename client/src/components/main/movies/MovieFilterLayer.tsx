f3689b2f1bfde951b3ed969830e0e122250c338a
slowteady <vbn0213@gmail.com>
2023-10-21 16:01:13 +0900

    feat: filter 값에 따라 값 변경돼도록 수정

--- client/src/components/main/movies/MovieFilterLayer.tsx
+++ client/src/components/main/movies/MovieFilterLayer.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuItem, Typography } from '@mui/material';
 import { MouseEvent, useState } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { FILTER_OPTION } from '../../../config/movie/movieConfig';
 import { filterState } from '../../../state/moviesState';
 import { Option } from '../../../types/movie';
@@ -10,7 +10,7 @@ const OPEN_ICON = 'eva:chevron-up-fill';
 const CLOSE_ICON = 'eva:chevron-down-fill';
 
 const MovieFilterLayer = () => {
-  const setFilter = useSetRecoilState(filterState);
+  const [filter, setFilter] = useRecoilState(filterState);
   const [open, setOpen] = useState(false);
   const [menuEl, setMenuEl] = useState<HTMLElement | null>(null);
 
@@ -34,7 +34,7 @@ const MovieFilterLayer = () => {
       <Button onClick={clickButton} color='inherit' endIcon={<Icon icon={open ? OPEN_ICON : CLOSE_ICON} />}>
         Sort By:&nbsp;
         <Typography component='span' variant='subtitle2' sx={{ color: 'text.secondary' }}>
-          {FILTER_OPTION[0].label}
+          {filter.label}
         </Typography>
       </Button>
       <Menu
