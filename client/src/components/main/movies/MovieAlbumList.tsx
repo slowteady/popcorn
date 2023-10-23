ef1a119282e99fc113cb225faf33d58bbd99b358
slowteady <vbn0213@gmail.com>
2023-10-23 18:07:17 +0900

    feat: 영화 앨범리스트 컴포넌트 추가

--- client/src/components/main/movies/MovieAlbumList.tsx
+++ client/src/components/main/movies/MovieAlbumList.tsx
@@ -1,26 +1,45 @@
-import { Grid } from '@mui/material';
-import { useRecoilValue } from 'recoil';
-import { filteredPath } from '../../../state/moviesState';
+import { Grid, styled } from '@mui/material';
+import { RefObject, forwardRef } from 'react';
+import { QueryStatus } from 'react-query';
+import { MoviesData } from '../../../types/movie';
 import { IconMsg } from '../../common/icon/IconMsg';
 import QueryStatusHandler from '../../hoc/QueryStatusHandler';
+import MovieCard from './MovieCard';
 
 const NODATA_MESSAGE = '데이터가 없습니다.';
 const NODATA_ICON = 'material-symbols:no-sim-outline-rounded';
 
-const MovieAlbumList = () => {
-  const path = useRecoilValue(filteredPath);
+interface MovieAlbumListProps {
+  status: QueryStatus;
+  movies: MoviesData[];
+  ref?: RefObject<HTMLDivElement>;
+}
 
+const MovieAlbumList = forwardRef<HTMLDivElement, MovieAlbumListProps>(({ movies, status }, ref) => {
   return (
-    <QueryStatusHandler status={'error'} sx={centerSx}>
-      <Grid container spacing={2}>
-        <IconMsg icon={NODATA_ICON} message={NODATA_MESSAGE} />
-      </Grid>
-    </QueryStatusHandler>
+    <>
+      <QueryStatusHandler status={status} sx={centerSx}>
+        <Grid container spacing={2}>
+          {movies && movies.length > 0 ? (
+            movies.map((movie, index) => {
+              return <MovieCard key={index} movies={movie} />;
+            })
+          ) : (
+            <IconMsg icon={NODATA_ICON} width={128} message={NODATA_MESSAGE} sx={centerSx} />
+          )}
+          {ref && <DummyDiv ref={ref} />}
+        </Grid>
+      </QueryStatusHandler>
+    </>
   );
-};
+});
 
 const centerSx = {
   height: '50vh'
 };
+
+const DummyDiv = styled('div')(() => ({
+  height: '40px'
+}));
 
 export default MovieAlbumList;
