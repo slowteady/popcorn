578a88506580842501696e705fc9dc8c3f2e1c40
slowteady <vbn0213@gmail.com>
2023-10-23 18:09:13 +0900

    feat: 영화 페이지 데이터 관리할 수 있도록 로직 수정

--- client/src/components/main/movies/MoviesIndex.tsx
+++ client/src/components/main/movies/MoviesIndex.tsx
@@ -1,8 +1,39 @@
 import { Stack, styled } from '@mui/material';
+import { AxiosResponse } from 'axios';
+import { useEffect, useRef } from 'react';
+import { InfiniteData, useInfiniteQuery } from 'react-query';
+import { useRecoilValue } from 'recoil';
+import useInfiniteScroll from '../../../hooks/useInfiniteScroll';
+import { getMovie } from '../../../service/movieService';
+import { filteredPath } from '../../../state/moviesState';
 import MovieAlbumList from './MovieAlbumList';
 import MovieFilterLayer from './MovieFilterLayer';
 
+const FIRST_PAGE = 1;
+
 const MoviesIndex = () => {
+  const path = useRecoilValue(filteredPath);
+  const dummyRef = useRef<HTMLDivElement | null>(null);
+  const isIntersecting = useInfiniteScroll(dummyRef);
+
+  const {
+    data: movies,
+    fetchNextPage,
+    hasNextPage,
+    status
+  } = useInfiniteQuery(['getFilterdMovies', path], ({ pageParam = FIRST_PAGE }) => getMovie(path, pageParam), {
+    getNextPageParam: ({ data }) => {
+      const { page, total_pages } = data;
+      return page < total_pages && page + 1;
+    }
+  });
+
+  useEffect(() => {
+    if (hasNextPage && isIntersecting) {
+      fetchNextPage();
+    }
+  }, [isIntersecting, hasNextPage]);
+
   return (
     <>
       <WrapStack>
@@ -10,9 +41,13 @@ const MoviesIndex = () => {
           <MovieFilterLayer />
         </Stack>
       </WrapStack>
-      <MovieAlbumList />
+      <MovieAlbumList status={status} ref={dummyRef} movies={movies ? transformMovieData(movies) : []} />
     </>
   );
+};
+
+const transformMovieData = (movies: InfiniteData<AxiosResponse<any, any>>) => {
+  return movies.pages.flatMap(({ data }) => data.results);
 };
 
 const WrapStack = styled(Stack)(({ theme }) => ({
