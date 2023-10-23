5e452670fc56bbcdf27527911291f4f84524cbe0
slowteady <vbn0213@gmail.com>
2023-10-23 18:11:41 +0900

    chore: src, alt 필수로 받도록 수정

--- client/src/components/main/movies/MovieImgBox.tsx
+++ client/src/components/main/movies/MovieImgBox.tsx
@@ -2,12 +2,12 @@ import { Box, SxProps } from '@mui/material';
 import { styled } from '@mui/system';
 
 interface MovieImgBoxProps {
-  alt?: string;
-  src?: string;
+  alt: string;
+  src: string;
   sx?: SxProps;
 }
 
-const MovieImgBox = ({ alt = '', src = '', sx }: MovieImgBoxProps) => {
+const MovieImgBox = ({ alt, src, sx }: MovieImgBoxProps) => {
   return (
     <Box sx={{ ...sx }}>
       <StyledMovieImg alt={alt} src={src} sx={{ ...sx }} />
