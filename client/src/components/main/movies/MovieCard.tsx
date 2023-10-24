4d527cd626d18435d2327b93697110eb11874cc1
slowteady <vbn0213@gmail.com>
2023-10-24 11:45:28 +0900

    feat: 무비 카드 컴포넌트 추가

---
+++ client/src/components/main/movies/MovieCard.tsx
@@ -0,0 +1,77 @@
+import { Box, Card, Stack, SxProps, Typography, styled } from '@mui/material';
+import { memo } from 'react';
+import { MOVIE_PATH } from '../../../api/movie/movieApiPaths';
+import { MoviesData } from '../../../types/movie';
+import MovieImgBox from './MovieImgBox';
+
+interface MovieCardProps {
+  movies: MoviesData;
+  sx?: SxProps;
+}
+
+const changeVoteAvg = (value: number) => {
+  return (Math.round(value * 10) / 10).toFixed(1);
+};
+
+const changePosterPath = (path: string) => {
+  const baseUrl = process.env.REACT_APP_IMAGE_BASE_URL;
+  const imagePath = MOVIE_PATH.IMAGE_SIZE_780;
+
+  return `${baseUrl}${imagePath}${path}`;
+};
+
+const MovieCard = ({ movies, sx }: MovieCardProps) => {
+  const { id, poster_path, release_date, title, vote_average, genre_ids } = movies;
+
+  return (
+    <>
+      <Card sx={{ ...defaultCardSx, ...sx }}>
+        <MovieImgBox src={changePosterPath(poster_path)} alt={title} />
+        <Stack spacing={2} sx={{ p: 3 }}>
+          <Box sx={{ color: 'inherit' }}>
+            <Typography title={title} variant='subtitle2' noWrap>
+              {title}
+            </Typography>
+            <FlexStack>
+              <Typography variant='subtitle1'>{release_date}</Typography>
+              <VoteAvgBox>
+                <VoteTypography>{changeVoteAvg(vote_average)}</VoteTypography>
+              </VoteAvgBox>
+            </FlexStack>
+          </Box>
+        </Stack>
+      </Card>
+    </>
+  );
+};
+
+const FlexStack = styled(Stack)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingTop: theme.spacing(2)
+}));
+
+const VoteAvgBox = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'rgb(0, 0, 0, 0.9)',
+  borderRadius: '50%',
+  height: '40px',
+  width: '40px'
+}));
+
+const VoteTypography = styled(Typography)(() => ({
+  fontsize: 15,
+  fontWeight: 800,
+  color: 'white'
+}));
+
+const defaultCardSx = {
+  cursor: 'pointer',
+  borderRadius: '12px'
+};
+
+export default memo(MovieCard);
