1d3b8e00fbc14814c098bf3bfca3cb8a7ffcc53e
slowteady <vbn0213@gmail.com>
2023-10-25 10:05:43 +0900

    feat: no image 추가

--- client/src/components/main/movies/utils/MovieImgBox.tsx
+++ client/src/components/main/movies/utils/MovieImgBox.tsx
@@ -1,5 +1,7 @@
 import { Box, SxProps } from '@mui/material';
 import { styled } from '@mui/system';
+import { useState } from 'react';
+import Icon from '../../../common/icon/Icon';
 
 interface MovieImgBoxProps {
   alt: string;
@@ -8,10 +10,24 @@ interface MovieImgBoxProps {
   imgSx?: SxProps;
 }
 
+const NO_IMG_ICON = 'carbon:no-image';
+
 const MovieImgBox = ({ alt, src, sx, imgSx }: MovieImgBoxProps) => {
+  const [hasError, setHasError] = useState(false);
+
+  const onError = () => {
+    setHasError(true);
+  };
+
   return (
     <Box sx={{ position: 'relative', ...defaultSx, ...sx }}>
-      <StyledMovieImg alt={alt} src={src} sx={{ ...imgSx }} />
+      {!hasError ? (
+        <NoImgBox>
+          <Icon icon={NO_IMG_ICON} width={84} />
+        </NoImgBox>
+      ) : (
+        <StyledMovieImg onError={onError} alt={alt} src={src} sx={{ ...imgSx }} />
+      )}
     </Box>
   );
 };
@@ -27,5 +43,15 @@ const StyledMovieImg = styled('img')({
 const defaultSx = {
   minHeight: '400px'
 };
+
+const NoImgBox = styled(Box)(() => ({
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  textAlign: 'center',
+  lineHeight: 30
+}));
 
 export default MovieImgBox;
