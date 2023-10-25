40a0960378ee423784611d7d964a141faa1e5f16
slowteady <vbn0213@gmail.com>
2023-10-25 17:30:27 +0900

    feat: 영화 검색 서비스 로직 추가

--- client/src/service/movieService.ts
+++ client/src/service/movieService.ts
@@ -1,4 +1,4 @@
-import { movieDataRequest } from '../api/movie/movieApiInstance';
+import { movieBasedRequest, movieDataRequest } from '../api/movie/movieApiInstance';
 import { MOVIE_PATH } from '../api/movie/movieApiPaths';
 
 export const getMovie = (path: string, page?: number) => {
@@ -11,4 +11,8 @@ export const getMovieDetail = (id: number) => {
 
 export const getMovieDetailCredits = (id: number) => {
   return movieDataRequest.get(`/${id}${MOVIE_PATH.CREDITS_PATH}`);
+};
+
+export const getSearchedMovie = (query: string, page: number) => {
+  return movieBasedRequest.get(`${MOVIE_PATH.SEARCH_PATH}`, { params: { query, page } });
 };
