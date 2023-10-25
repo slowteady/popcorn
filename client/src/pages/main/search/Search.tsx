4a8d42c2b8b4544dedb5642895d837feff4c0c21
slowteady <vbn0213@gmail.com>
2023-10-25 10:52:08 +0900

    feat: search 컴포넌트 기본 구조 추가

--- client/src/pages/main/search/Search.tsx
+++ client/src/pages/main/search/Search.tsx
@@ -1,5 +1,21 @@
+import { Container, Typography } from '@mui/material';
+import Helmet from '../../../components/common/Helmet';
+import SearchIndex from '../../../components/main/search/SearchIndex';
+
+const PAGE_TITLE = 'Search';
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <>
+      <Helmet text={PAGE_TITLE} />
+      <Container>
+        <Typography variant='h4' sx={{ mb: 5 }}>
+          {PAGE_TITLE}
+        </Typography>
+        <SearchIndex />
+      </Container>
+    </>
+  );
 };
 
 export default Search;
