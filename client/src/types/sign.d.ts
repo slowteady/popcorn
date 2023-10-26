0a71efc55de25b95cb8bc78a70c6b851600a6e73
slowteady <vbn0213@gmail.com>
2023-10-26 13:12:10 +0900

    format: 포매터 적용

--- client/src/types/sign.d.ts
+++ client/src/types/sign.d.ts
@@ -2,10 +2,12 @@ export interface BasicForm {
   email: string;
   password: string;
 }
+
 export interface SignUpForm extends BasicForm {
   name: string;
   confirmPassword: string;
 }
+
 export interface SignInForm extends BasicForm {
   rememberMe: boolean;
 }
