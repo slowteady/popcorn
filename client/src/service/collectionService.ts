26ed73c56cc4b7d58118767355a5c76f1faf9dbc
slowteady <vbn0213@gmail.com>
2023-10-26 17:56:01 +0900

    feat: 컬렉션 디테일 요청 서비스 추가

--- client/src/service/collectionService.ts
+++ client/src/service/collectionService.ts
@@ -1,5 +1,9 @@
-import { collectionBasedRequest } from '../api/apiInstance';
+import { collectionBasedRequest, collectionDetailRequest } from '../api/apiInstance';
 
 export const getCollection = (page: number, limit: number) => {
   return collectionBasedRequest.get(`/${page}`, { params: { limit } });
+};
+
+export const getCollectionDetail = (id: string, page: number, limit: number) => {
+  return collectionDetailRequest.get(`/${id}`, { params: { page, limit } });
 };
