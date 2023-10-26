e59e891465b5864129a75ee8342bdf7873b9f311
slowteady <vbn0213@gmail.com>
2023-10-26 13:11:53 +0900

    feat: 컬렉션 인덱스 컴포넌트 추가

--- client/src/components/main/collection/CollectionIndex.tsx
+++ client/src/components/main/collection/CollectionIndex.tsx
@@ -1,5 +1,38 @@
+import { Button, Container, Stack, styled } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import paths from '../../../config/routes/paths';
+import Icon from '../../common/icon/Icon';
+import CollectionBoard from './list/CollectionBoard';
+
+const ADD_BUTTON_ICON = 'eva:plus-fill';
+
+const { main } = paths.main;
+const { index, add } = paths.main.collection;
+
 const CollectionIndex = () => {
-  return <div>CollectionIndex</div>;
+  const navigate = useNavigate();
+
+  const naviToAdd = () => {
+    navigate(`${main}${index}${add}`);
+  };
+
+  return (
+    <Container sx={{ pl: '0 !important' }}>
+      <FlexStack>
+        <Button onClick={naviToAdd} variant='contained' startIcon={<Icon icon={ADD_BUTTON_ICON} />}>
+          New Collection
+        </Button>
+      </FlexStack>
+      <CollectionBoard />
+    </Container>
+  );
 };
+
+const FlexStack = styled(Stack)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-end'
+}));
 
 export default CollectionIndex;
