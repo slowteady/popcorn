e68cdb7a9a0e3b72e84e02e49cacb8471baf13ca
slowteady <vbn0213@gmail.com>
2023-10-27 15:37:02 +0900

    feat: 컬렉션 추가 컴포넌트 추가

--- client/src/pages/main/collection/CollectionAdd.tsx
+++ client/src/pages/main/collection/CollectionAdd.tsx
@@ -1,6 +1,47 @@
+import { Button, Container, Stack, Typography, styled } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import Icon from '../../../components/common/icon/Icon';
+import CollectionAddBody from '../../../components/main/collection/add/CollectionAddBody';
+import paths from '../../../config/routes/paths';
+
+const LIST_ICON = 'ph:list-fill';
+const PAGE_TITLE = 'Collection';
+
+const { main } = paths;
+const { index } = main.collection;
 
 const CollectionAdd = () => {
-  return <div>CollectionAdd</div>;
+  const navigate = useNavigate();
+
+  const naviToList = () => {
+    navigate(`${main}${index}`);
+  };
+
+  return (
+    <Container>
+      <Typography variant='h4' sx={{ mb: 3 }}>
+        {PAGE_TITLE}
+      </Typography>
+      <FlexStack>
+        <Button
+          onClick={naviToList}
+          variant='contained'
+          startIcon={<Icon icon={LIST_ICON} />}
+          sx={{ backgroundColor: '#3e4857' }}
+        >
+          List
+        </Button>
+      </FlexStack>
+      <CollectionAddBody />
+    </Container>
+  );
 };
 
-export default CollectionAdd
+const FlexStack = styled(Stack)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-end'
+}));
+
+export default CollectionAdd;
