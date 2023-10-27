9b9a6c940339c5bf2440c00e23c9b50832498551
slowteady <vbn0213@gmail.com>
2023-10-27 19:42:34 +0900

    feat: 아이콘 레이어 버튼 컴포넌트 추가

--- client/src/pages/main/collection/CollectionAdd.tsx
+++ client/src/pages/main/collection/CollectionAdd.tsx
@@ -1,9 +1,12 @@
 import { Button, Container, Stack, Typography, styled } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Icon from '../../../components/common/icon/Icon';
+import IconLayerButton from '../../../components/layouts/button/IconLayerButton';
 import CollectionAddBody from '../../../components/main/collection/add/CollectionAddBody';
+import CollectionListBody from '../../../components/main/collection/list/CollectionListBody';
 import paths from '../../../config/routes/paths';
 
+const CART_ICON = 'el:shopping-cart-sign';
 const LIST_ICON = 'ph:list-fill';
 const PAGE_TITLE = 'Collection';
 
@@ -23,6 +26,15 @@ const CollectionAdd = () => {
         {PAGE_TITLE}
       </Typography>
       <FlexStack>
+        <IconLayerButton
+          icon={CART_ICON}
+          width={35}
+          anchorOrigin={{ vertical: 'bottom', horizontal: -200 }}
+          transformOrigin={{ vertical: 'top', horizontal: 200 }}
+          sx={{ mr: 2 }}
+        >
+          <CollectionListBody />
+        </IconLayerButton>
         <Button
           onClick={naviToList}
           variant='contained'
@@ -37,11 +49,12 @@ const CollectionAdd = () => {
   );
 };
 
-const FlexStack = styled(Stack)(() => ({
+const FlexStack = styled(Stack)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'flex-end'
+  justifyContent: 'flex-end',
+  margin: theme.spacing(0, 2.5)
 }));
 
 export default CollectionAdd;
