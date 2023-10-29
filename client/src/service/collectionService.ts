51ade2c016717892659fc0feec7335188136e7a4
slowteady <vbn0213@gmail.com>
2023-10-29 20:49:03 +0900

    feat: 컬렉션 등록 서비스 로직 추가

--- client/src/service/collectionService.ts
+++ client/src/service/collectionService.ts
@@ -1,4 +1,10 @@
 import { collectionBasedRequest, collectionDetailRequest } from '../api/apiInstance';
+import { MoviesData } from '../types/movie';
+
+interface RegisterCollecionBody {
+  collectionTitle: string;
+  movie: MoviesData[];
+}
 
 export const getCollection = (page: number, limit: number) => {
   return collectionBasedRequest.get(`/${page}`, { params: { limit } });
@@ -10,4 +16,8 @@ export const getCollectionDetail = (id: string, page: number, limit: number) => 
 
 export const deleteCollection = (id: string) => {
   return collectionBasedRequest.delete('', { params: { id } });
+};
+
+export const registerCollection = (body: RegisterCollecionBody) => {
+  return collectionBasedRequest.post('/register', body);
 };
