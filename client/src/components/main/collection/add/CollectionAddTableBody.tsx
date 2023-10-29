8d4713d2ed061de08115c283f90352b5c402bda9
slowteady <vbn0213@gmail.com>
2023-10-29 17:21:26 +0900

    feat: 제목 클릭 시 모달 창 출력 구현

--- client/src/components/main/collection/add/CollectionAddTableBody.tsx
+++ client/src/components/main/collection/add/CollectionAddTableBody.tsx
@@ -1,9 +1,11 @@
 import { TableBody, TableCell, TableRow, Typography } from '@mui/material';
-import { MouseEvent } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { ADD_TABLE_CONF } from '../../../../config/layout/tableConfig';
 import { checkedMoviesState } from '../../../../state/collectionState';
+import { changePosterPath } from '../../../../utils/dataTransForm';
 import Icon from '../../../common/icon/Icon';
+import MovieModal from '../../movies/modal/MovieModal';
 
 interface CollectionAddTableBodyProps {
   page: number;
@@ -14,8 +16,17 @@ const { ROWSPERPAGE } = ADD_TABLE_CONF;
 
 const CollectionAddTableBody = ({ page }: CollectionAddTableBodyProps) => {
   const [checkedMovies, setCheckedMovies] = useRecoilState(checkedMoviesState);
+  const [open, setOpen] = useState(false);
+  const idRef = useRef<number>(0);
+  const posterPathRef = useRef<string>('');
   const startIndex = (page - 1) * ROWSPERPAGE;
   const endIndex = page * ROWSPERPAGE;
+
+  const openModal = (id: number, posterPath: string) => {
+    idRef.current = id;
+    posterPathRef.current = posterPath;
+    setOpen(true);
+  };
 
   const doDelete = (e: MouseEvent, id: number) => {
     e.stopPropagation();
@@ -23,31 +34,43 @@ const CollectionAddTableBody = ({ page }: CollectionAddTableBodyProps) => {
     setCheckedMovies((prevCheckedMovies) => prevCheckedMovies.filter((movies) => movies.id !== id));
   };
 
-  return (
-    <TableBody>
-      {checkedMovies.length > 0 &&
-        checkedMovies.slice(startIndex, endIndex).map((movie) => {
-          const { id, title, release_date } = movie;
+  const modalData = () => {
+    const id = idRef.current;
+    const posterPath = changePosterPath(posterPathRef.current);
 
-          return (
-            <TableRow hover key={id} tabIndex={-1} role='checkbox'>
-              <TableCell align='left'>
-                <Icon onClick={(e) => doDelete(e, id)} icon={DELETE_ICON} sx={deleteIconSx} />
-              </TableCell>
-              <TableCell align='left'>
-                <Typography variant='subtitle2' noWrap sx={titleSx}>
-                  {title}
-                </Typography>
-              </TableCell>
-              <TableCell align='left'>
-                <Typography variant='subtitle2' noWrap>
-                  {release_date}
-                </Typography>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-    </TableBody>
+    return { id, posterPath };
+  };
+
+  return (
+    <>
+      <TableBody>
+        {checkedMovies.length > 0 &&
+          checkedMovies.slice(startIndex, endIndex).map((movie) => {
+            const { id, title, release_date, poster_path } = movie;
+
+            return (
+              <>
+                <TableRow hover key={id} tabIndex={-1} role='checkbox'>
+                  <TableCell align='left'>
+                    <Icon onClick={(e) => doDelete(e, id)} icon={DELETE_ICON} sx={deleteIconSx} />
+                  </TableCell>
+                  <TableCell align='left'>
+                    <Typography onClick={() => openModal(id, poster_path)} variant='subtitle2' noWrap sx={titleSx}>
+                      {title}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align='left'>
+                    <Typography variant='subtitle2' noWrap>
+                      {release_date}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              </>
+            );
+          })}
+      </TableBody>
+      <MovieModal open={open} setOpen={setOpen} movie={modalData()} />
+    </>
   );
 };
 
