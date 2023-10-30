31dbca4392146e8ea9b4b1cba9731e76c8039a08
slowteady <vbn0213@gmail.com>
2023-10-30 20:22:30 +0900

    fix: prod 환경에 맞춰 빌드 수정

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -1,48 +1,51 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    app: "./src/index.tsx", // 처음 시작할 파일
+    app: './src/index.tsx' // 처음 시작할 파일
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/, // 확장자
-        use: "ts-loader", // ts -> js 로 컴파일해줌
-        exclude: /node_modules/, // 제외할 폴더
+        use: 'ts-loader', // ts -> js 로 컴파일해줌
+        exclude: /node_modules/ // 제외할 폴더
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "images/[name].[ext]",
-            },
-          },
-        ],
-      },
-    ],
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"], // 모듈을 찾을 때 확장자 순서
+    extensions: ['.tsx', '.ts', '.js', '.jsx'] // 모듈을 찾을 때 확장자 순서
   },
   output: {
-    path: path.join(__dirname, "dist"), // 빌드하면 만들어질 경로
-    filename: "bundle.js", // 파일 이름
-    publicPath: "/",
+    path: path.join(__dirname, 'dist'), // 빌드하면 만들어질 경로
+    filename: 'bundle.js', // 파일 이름
+    publicPath: '/'
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "public/index.html",
+      template: 'public/index.html'
     }),
     new CopyWebpackPlugin({
-      patterns: [
-        { from: "public", to: ".", globOptions: { ignore: ["**/index.html"] } },
-      ],
+      patterns: [{ from: 'public', to: '.', globOptions: { ignore: ['**/index.html'] } }]
     }),
-  ],
+    new Dotenv({
+      path: './.env.production',
+      safe: true
+    })
+  ]
 };
